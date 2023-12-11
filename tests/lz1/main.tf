0fd118bd2240915a4007393860e7c86a87d6e241
Dimitar Rusev <drusev@efellows.bg>
2023-12-11 15:02:25 +0200

    Move variables and clear test state files

--- tests/lz1/main.tf
+++ tests/lz1/main.tf
@@ -8,17 +8,6 @@ terraform {
 
 module "tinycaf" {
   source = "../../src"
-  resource_groups = {
-    rg_01 = {
-      name     = "rg-tinycaf-test-01"
-      location = "westeurope"
-      tags = {
-        "Owner"    = "drusev@efellows.bg"
-        "DeadLine" = "2023-12-05"
-        "Project"  = "tinycaf / eFellow Internal Infra"
-      }
-    }
-  }
   config = {
     # no container definitions means use local
     state_file = "lz1.tfstate"
--- tests/lz2/main.tf
+++ tests/lz2/main.tf
@@ -9,38 +9,6 @@ terraform {
 module "tinycaf" {
   source = "../../src"
 
-  managed_identities = {
-    id_test_01 = {
-      resource_group = {
-        ref       = "rg_01"
-        state_ref = "lz1"
-      }
-      name = "id-tinycaf-test-01"
-    }
-    id_test_02 = {
-      resource_group_ref = "rg_02"
-      name               = "id-tinycaf-test-02"
-    }
-    id_test_03 = {
-      resource_group = {
-        ref       = "rg_01"
-        state_ref = "lz1"
-      }
-      name = "id-tinycaf-test-03"
-    }
-  }
-  resource_groups = {
-    rg_02 = {
-      name     = "rg-tinycaf-test-02"
-      location = "westeurope"
-      tags = {
-        "Owner"    = "drusev@efellows.bg"
-        "DeadLine" = "2023-12-05"
-        "Project"  = "tinycaf / eFellow Internal Infra"
-      }
-    }
-  }
-
   config = {
     # TODO: find a way to enforce
     state_file = "lz2.tfstate"
