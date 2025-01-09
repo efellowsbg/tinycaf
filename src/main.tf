73dfafb6b3a895baa37c52035027ba5c09a6027c
Dimitar Rusev <drusev@efellows.bg>
2025-01-09 15:08:33 +0200

    Remove unused files

--- src/main.tf
+++ src/main.tf
@@ -1,51 +1,12 @@
-# TODO: location abstraction map
-# TODO: custom / overridable abstraction map
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.14.0"
+    }
+  }
+}
 
 provider "azurerm" {
   features {}
-}
-
-locals {
-  ref = trimsuffix(var.config.state_file, ".tfstate")
-}
-
-data "terraform_remote_state" "remote" {
-  for_each = coalesce(var.config.remote_states, {})
-
-  backend = can(each.value.container) ? "remote" : "local"
-  config = can(each.value.container) ? {
-    # TODO: use a separate settings landingzone, or allow setting them explicitly
-    resource_group_name  = "rg-shd-infra"
-    storage_account_name = "stshdeflinfra"
-    container_name       = each.value.container
-    key                  = each.value.state_file
-  } : { path = each.value.state_file }
-}
-
-resource "azurerm_resource_group" "main" {
-  for_each = var.resource_groups
-
-  name     = each.value.name
-  location = each.value.location
-  tags     = try(each.value.tags, null)
-}
-
-resource "azurerm_user_assigned_identity" "main" {
-  for_each = var.managed_identities
-
-  name                = each.value.name
-  resource_group_name = local.all_resource_groups[try(each.value.resource_group.state_ref, local.ref)][try(each.value.resource_group.ref, each.value.resource_group_ref)].name
-  location            = local.all_resource_groups[try(each.value.resource_group.state_ref, local.ref)][try(each.value.resource_group.ref, each.value.resource_group_ref)].location
-}
-
-module "users" {
-  source   = "./modules/user"
-  for_each = var.users
-
-  ref      = each.key
-  settings = each.value
-}
-
-output "objects" {
-  value = local.objects
 }
--- src/objects_all.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  all_resource_groups    = merge({ (local.ref) = local.local_resource_groups }, local.remote_resource_groups)
-  all_managed_identities = merge({ (local.ref) = local.local_managed_identities }, local.remote_managed_identities)
-  all_users              = merge({ (local.ref) = local.local_users }, local.remote_users)
-}
