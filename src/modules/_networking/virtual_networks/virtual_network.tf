ac3da39b44e2a355baff99fe735950ca9ae8c91e
Dimitar Rusev <drusev@efellows.bg>
2025-01-13 15:59:16 +0200

    Formatting

--- src/modules/_networking/virtual_networks/virtual_network.tf
+++ src/modules/_networking/virtual_networks/virtual_network.tf
@@ -2,6 +2,8 @@ resource "azurerm_virtual_network" "main" {
   name                = var.settings.name
   location            = local.location
   resource_group_name = local.resource_group_name
-  address_space       = var.settings.cidr
-  tags                = try(local.tags, null)
+
+  address_space = var.settings.cidr
+
+  tags = try(local.tags, null)
 }
