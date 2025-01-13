c8931f1e3cdd4d00dfdbf655dccd600da0143bf0
lyudmililchev92 <lyudmil.ilchev@teladochealth.com>
2025-01-13 17:27:08 +0200

    add direction checker

--- src/modules/_networking/virtual_networks/_variables.tf
+++ src/modules/_networking/virtual_networks/_variables.tf
@@ -14,3 +14,13 @@ variable "resources" {
   })
   description = "All required resources"
 }
+
+variable "direction" {
+  description = "Peering direction: '1to2', '2to1', or 'both'. Optional. Defaults to 'both' if unset."
+  type        = string
+  default     = null
+  validation {
+    condition     = var.direction == null || contains(["1to2", "2to1", "both"], var.direction)
+    error_message = "Allowed values are '1to2', '2to1', or 'both'."
+  }
+}
