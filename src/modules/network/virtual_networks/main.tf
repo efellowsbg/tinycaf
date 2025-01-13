36bbadcbe2029a4de2b0e97a489dfbe2bda435d5
lyudmililchev92 <lyudmil.ilchev@teladochealth.com>
2025-01-13 14:47:07 +0200

    add delegation and service endpoint to subnet

--- src/modules/network/virtual_networks/main.tf
+++ src/modules/network/virtual_networks/main.tf
@@ -12,6 +12,16 @@ resource "azurerm_virtual_network" "vnet" {
       name             = subnet.value.name
       address_prefixes = subnet.value.address_prefixes
       security_group   = try(subnet.value.nsg_ref, null) == null ? null : var.resources.network_security_group[subnet.value.nsg_ref].id
+      service_endpoints = try(subnet.value.service_endpoints, null)
+      dynamic "delegation" {
+        for_each = try([subnet.value.delegation], [])  # Include only if delegation is defined
+        content {
+          name = delegation.value.name
+          service_delegation {
+            name    = delegation.value.service_delegation.name
+            actions = try(delegation.value.service_delegation.actions, null)
+          }
+        }
     }
   }
 }
