d7686c28ab5f5b16fe040de5b64c8deb24b54fdf
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-14 17:59:09 +0200

    test

--- src/modules/_networking/virtual_networks/_outputs.tf
+++ src/modules/_networking/virtual_networks/_outputs.tf
@@ -14,6 +14,6 @@ output "subnet_ids" {
   description = "IDs of all subnets grouped by VNet and Subnet"
   value = {
     for subnet_key, subnet in azurerm_subnet.main :
-    "${subnet.virtual_network_name}.${subnet.name}" => subnet.id
+    "${azurerm_virtual_network.main.name}.${subnet.name}" => subnet.id
   }
 }
