a970f84f926f8ff2d0b2605303956e3ddddd6a4f
Borislav Raynov <braynov@efellows.bg>
2025-01-14 14:54:24 +0200

    Refactored ouput

--- src/modules/_networking/virtual_networks/_outputs.tf
+++ src/modules/_networking/virtual_networks/_outputs.tf
@@ -9,17 +9,6 @@ output "name" {
 output "resource_group_name" {
   value = azurerm_virtual_network.main.resource_group_name
 }
-output "id" {
-  value = azurerm_virtual_network.main.id
-}
-
-output "name" {
-  value = azurerm_virtual_network.main.name
-}
-
-output "resource_group_name" {
-  value = azurerm_virtual_network.main.resource_group_name
-}
 
 output "subnet_id" {
   value = { for key, subnet in azurerm_subnet.main : key => subnet.id }
