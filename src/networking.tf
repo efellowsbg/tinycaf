12a7ca3ee63479a2747a2322541ebd94492b551f
lyudmililchev92 <lyudmil.ilchev@teladochealth.com>
2025-01-14 10:10:47 +0200

    add vnet peering tf file

--- src/networking.tf
+++ src/networking.tf
@@ -9,15 +9,3 @@ module "virtual_networks" {
     resource_groups = module.resource_groups
   }
 }
-
-
-module "vnet_peering" {
-  source = "./modules/_networking/vnet_peering"
-  for_each = var.vnet_peering
-  global_settings = var.global_settings
-  settings = each.value
-  resources = {
-    virtual_networks = module.virtual_networks
-    resource_groups = module.resource_groups
-  }
-}
---
+++ src/vnet_peering.tf
@@ -0,0 +1,9 @@
+module "vnet_peering" {
+  source = "./modules/_networking/vnet_peering"
+  for_each = var.vnet_peering
+  global_settings = var.global_settings
+  settings = each.value
+  resources = {
+    virtual_networks = module.virtual_networks
+  }
+}
