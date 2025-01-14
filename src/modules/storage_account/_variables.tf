58bca16739324ddbb2d487be244d76fbf63a8532
Borislav Raynov <braynov@efellows.bg>
2025-01-14 16:19:31 +0200

    Change module resources

--- src/modules/storage_account/_variables.tf
+++ src/modules/storage_account/_variables.tf
@@ -9,7 +9,7 @@ variable "settings" {
 variable "resources" {
   type = object({
     resource_groups = map(any)
-    vnets = map(any)
+    virtual_networks = map(any)
   })
   description = "All required resources"
 }
