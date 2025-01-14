4362e38f5413c4bb52b42ba0dad31c1442e5d9bd
lyudmililchev92 <lyudmil.ilchev@teladochealth.com>
2025-01-14 10:21:48 +0200

    add outputs file

--- src/modules/_networking/vnet_peering/vnet_peering.tf
+++ src/modules/_networking/vnet_peering/vnet_peering.tf
@@ -1,17 +1,10 @@
 resource "azurerm_virtual_network_peering" "main" {
-  for_each = {
+ for_each = {
     for key, peering in local.peerings : key => peering
-    if (var.direction == "both" || var.direction == peering.direction) &&
-       peering.source_vnet != null && peering.target_vnet != null
+    if var.direction == "both" || var.direction == peering.direction
   }
-
-  name                      = "${each.value.source_vnet.name}-to-${each.value.target_vnet.name}"
-  resource_group_name       = each.value.source_vnet.resource_group_name
-  virtual_network_name      = each.value.source_vnet.name
+  name                = "${each.value.source_vnet.name}-to-${each.value.target_vnet.name}"
+  resource_group_name = each.value.source_vnet.resource_group_name
+  virtual_network_name = each.value.source_vnet.name
   remote_virtual_network_id = each.value.target_vnet.id
-
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = false
-  allow_gateway_transit        = false
-  use_remote_gateways          = false
 }
