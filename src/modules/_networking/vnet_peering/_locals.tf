6236ef931d9f679bc391c9a843b65b067d3e6e18
lyudmililchev92 <lyudmil.ilchev@teladochealth.com>
2025-01-14 10:17:52 +0200

    reformat the module

--- src/modules/_networking/vnet_peering/_locals.tf
+++ src/modules/_networking/vnet_peering/_locals.tf
@@ -1,20 +1,20 @@
 locals {
-  vnet1 = var.resources.virtual_networks[var.settings.vnet1_ref]
-  vnet2 = var.resources.virtual_networks[var.settings.vnet2_ref]
-}
+  vnet1 = lookup(var.resources.virtual_networks, var.settings.vnet1_ref, null)
+  vnet2 = lookup(var.resources.virtual_networks, var.settings.vnet2_ref, null)
 
-locals {
   peerings = {
-    "vnet1_to_vnet2" = {
-      source_vnet = local.vnet1
-      target_vnet = local.vnet2
-      direction   = "1to2"
-    }
-    "vnet2_to_vnet1" = {
-      source_vnet = local.vnet2
-      target_vnet = local.vnet1
-      direction   = "2to1"
-    }
+    for key, peering in {
+      "vnet1_to_vnet2" = {
+        source_vnet = local.vnet1
+        target_vnet = local.vnet2
+        direction   = "1to2"
+      }
+      "vnet2_to_vnet1" = {
+        source_vnet = local.vnet2
+        target_vnet = local.vnet1
+        direction   = "2to1"
+      }
+    } : key => peering
+    if peering.source_vnet != null && peering.target_vnet != null
   }
 }
-
