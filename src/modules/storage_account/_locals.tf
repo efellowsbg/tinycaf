86e29280375c1b1c917dd8068d4eec3a3d196c4c
Borislav Raynov <braynov@efellows.bg>
2025-01-15 16:10:48 +0200

    Remove comment

--- src/modules/storage_account/_locals.tf
+++ src/modules/storage_account/_locals.tf
@@ -1,14 +1,14 @@
 locals {
-  resource_group = var.resources.resource_groups[var.settings.resource_group_ref]
+  resource_group      = var.resources.resource_groups[var.settings.resource_group_ref]
   resource_group_name = local.resource_group.name
-  location = local.resource_group.location
+  location            = local.resource_group.location
 
   # vnet = var.resources.virtual_networks[var.settings.network.config.vnet_ref]
   # subnet_id = local.vnet.subnets[var.settings.network.subnet_ref].id
 
   # subnet_id = [for config in values(local.storage_account_network) : local.virtual_networks[config.vnet_ref].subnets[config.subnet_ref].id]
   # subnet_id = [
-  #   for config in var.settings.network : 
+  #   for config in var.settings.network :
   #   var.resources.virtual_networks[var.settings.network.config.value.vnet_ref].subnets[var.settings.network.config.value.subnet_ref].id
   # ]
 
@@ -19,7 +19,7 @@ locals {
   # ])
 
   # subnet_id = flatten([
-  #   for key in try(var.settings.network, []) : 
+  #   for key in try(var.settings.network, []) :
   #   var.resources.virtual_networks[key.vnet_ref].subnets[key.subnet_ref].id
   # ])
 
--- src/modules/storage_account/main.tf
+++ src/modules/storage_account/main.tf
@@ -9,9 +9,6 @@ resource "azurerm_storage_account" "main" {
     default_action             = try(var.settings.default_action, "Deny")
     ip_rules                   = try(var.settings.ip_rules, null)
     virtual_network_subnet_ids = local.subnet_ids
-    # virtual_network_subnet_ids = try(var.settings.network, null) == null ? null : [
-    #     for key, value in var.settings.network : can(value.subnet_id) ? value.subnet_id : var.resources.virtual_networks[value.vnet_ref].subnets[value.subnet_ref].id
-    #   ]
   }
 
   tags = try(local.tags, null)
