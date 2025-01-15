3997337887e181a5f7b13d6ae3b69ae2fc8d89fa
Borislav Raynov <braynov@efellows.bg>
2025-01-15 16:22:58 +0200

    Add new line at the end.

--- src/storage_account.tf
+++ src/storage_account.tf
@@ -1,12 +1,12 @@
 module "storage_accounts" {
-  source = "./modules/storage_account"
+  source   = "./modules/storage_account"
   for_each = var.storage_accounts
 
-  settings = each.value
+  settings        = each.value
   global_settings = var.global_settings
 
   resources = {
-    resource_groups = module.resource_groups
+    resource_groups  = module.resource_groups
     virtual_networks = module.virtual_networks
   }
 }
