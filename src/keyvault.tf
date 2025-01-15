71a0fa54b38bb5c8add2d1a2f0513b0493ba25ec
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-15 18:03:38 +0200

    fix pre-commit

--- src/keyvault.tf
+++ src/keyvault.tf
@@ -4,9 +4,9 @@ module "keyvaults" {
 
   settings        = each.value
   global_settings = var.global_settings
-  tenant_id = var.tenant_id
+  tenant_id       = var.tenant_id
   resources = {
-    resource_groups = module.resource_groups
+    resource_groups  = module.resource_groups
     virtual_networks = module.virtual_networks
   }
 }
--- src/modules/_security/keyvault/keyvault.tf
+++ src/modules/_security/keyvault/keyvault.tf
@@ -1,16 +1,16 @@
 resource "azurerm_key_vault" "main" {
-  name                = var.settings.name
-  resource_group_name = local.resource_group_name
-  location            = local.location
+  name                        = var.settings.name
+  resource_group_name         = local.resource_group_name
+  location                    = local.location
   enabled_for_disk_encryption = try(var.settings.enabled_for_disk_encryption, true)
-  tenant_id = local.tenant_id
-  soft_delete_retention_days = try(var.settings.soft_delete_retention_days, 7)
+  tenant_id                   = local.tenant_id
+  soft_delete_retention_days  = try(var.settings.soft_delete_retention_days, 7)
   purge_protection_enabled    = try(var.settings.purge_protection_enabled, false)
-  sku_name = try(var.settings.sku_name, "standard")
-  enable_rbac_authorization = try(var.settings.enable_rbac_authorization, true)
+  sku_name                    = try(var.settings.sku_name, "standard")
+  enable_rbac_authorization   = try(var.settings.enable_rbac_authorization, true)
   network_acls {
     default_action             = try(var.settings.network.default_action, null)
-    bypass         = try(var.settings.network.bypass, "AzureServices")
+    bypass                     = try(var.settings.network.bypass, "AzureServices")
     ip_rules                   = try(var.settings.network.allowed_ips, null)
     virtual_network_subnet_ids = local.subnet_ids
   }
