ff9ec4c2d265e3703cbd279177c54e16410b7564
Borislav Raynov <braynov@efellows.bg>
2025-01-15 15:07:30 +0200

    Refactor subnet_id

--- src/modules/storage_account/_locals.tf
+++ src/modules/storage_account/_locals.tf
@@ -4,29 +4,19 @@ locals {
   location = local.resource_group.location
 
   # vnet = var.resources.virtual_networks[var.settings.network.config.vnet_ref]
-
   # subnet_id = local.vnet.subnets[var.settings.network.subnet_ref].id
-  # subnet_id = [for config in values(local.storage_account_network) : local.virtual_networks[config.vnet_ref].subnets[config.subnet_ref].id]
-  subnet_id = [
-    for config in var.settings.network : 
-    var.resources.virtual_networks[var.settings.network.config.value.vnet_ref].subnets[var.settings.network.config.value.subnet_ref].id
-  ]
 
+  # subnet_id = [for config in values(local.storage_account_network) : local.virtual_networks[config.vnet_ref].subnets[config.subnet_ref].id]
   # subnet_id = [
   #   for config in var.settings.network : 
-  #   {vnet = config.vnet_ref
-    
-  #   }
+  #   var.resources.virtual_networks[var.settings.network.config.value.vnet_ref].subnets[var.settings.network.config.value.subnet_ref].id
   # ]
 
-#  subnet_id = flatten([
-#     for config, account_data in var.storage_accounts : [
-#       for config_name, config in account_data.network : {
-#         vnet_ref   = config.vnet_ref
-#         subnet_ref = config.subnet_ref
-#       }
-#     ]
-#   ])
+  subnet_id = flatten([
+    for vnet_ref, subnet_ref in try(var.settings.network, []) : [
+      var.resources.virtual_networks[vnet_ref].subnets[subnet_ref].id
+    ]
+  ])
 
   tags = merge(
     var.global_settings.tags,
