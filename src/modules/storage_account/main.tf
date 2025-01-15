398c0d0ebd49791e68342ef242c045340a4193fa
Borislav Raynov <braynov@efellows.bg>
2025-01-15 15:47:08 +0200

    Fix typo

--- src/modules/storage_account/main.tf
+++ src/modules/storage_account/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_storage_account" "main" {
     default_action             = try(var.settings.default_action, "Deny")
     ip_rules                   = try(var.settings.ip_rules, null)
     virtual_network_subnet_ids = try(var.settings.network, null) == null ? null : [
-        for key, value in var.settings.network : can(value.subnet_id) ? value.subnet_id : var.resources.virtual_networkstry[value.vnet_ref].subnets[value.subnet_ref].id
+        for key, value in var.settings.network : can(value.subnet_id) ? value.subnet_id : var.resources.virtual_networks[value.vnet_ref].subnets[value.subnet_ref].id
       ]
     }
 
