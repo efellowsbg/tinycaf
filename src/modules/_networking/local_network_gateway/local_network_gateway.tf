bd088f8b73ab8d1555013eb5705a0fdcc28bb6aa
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-15 11:03:25 +0200

    add try in the module

--- src/modules/_networking/local_network_gateway/local_network_gateway.tf
+++ src/modules/_networking/local_network_gateway/local_network_gateway.tf
@@ -2,7 +2,7 @@ resource "azurerm_local_network_gateway" "main" {
   name                = var.settings.name
   location            = local.location
   resource_group_name = local.resource_group_name
-  gateway_address     = var.settings.gateway_address
+  gateway_address     = try(var.settings.gateway_address, null)
   address_space       = var.settings.cidr
-  gateway_fqdn = var.settings.gateway_fqdn
+  gateway_fqdn = try(var.settings.gateway_fqdn, null)
 }
