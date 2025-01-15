3f73b26263d5bf8ca67252a0a8e00fab834cdffd
Dimitar Rusev <drusev@efellows.bg>
2025-01-15 11:54:55 +0200

    Remove condition from variables

--- src/modules/_networking/local_network_gateway/_variables.tf
+++ src/modules/_networking/local_network_gateway/_variables.tf
@@ -4,13 +4,6 @@ variable "global_settings" {
 
 variable "settings" {
   description = "All the configuration for this resource"
-  validation {
-    condition = (
-      (try(var.settings.gateway_address, null) != null && try(var.settings.gateway_address, "") != "" && try(var.settings.gateway_fqdn, null) == null) ||
-      (try(var.settings.gateway_fqdn, null) != null && try(var.settings.gateway_fqdn, "") != "" && try(var.settings.gateway_address, null) == null)
-    )
-    error_message = "You must specify **either** 'gateway_address' **or** 'gateway_fqdn', but not both and not neither."
-  }
 }
 
 variable "resources" {
