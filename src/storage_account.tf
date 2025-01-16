b766096553997a07a6a4c50fb9a25f3a4db5b99e
Borislav Raynov <braynov@efellows.bg>
2025-01-16 10:49:42 +0200

    Add resource for storage accounts

--- src/storage_account.tf
+++ src/storage_account.tf
@@ -8,5 +8,6 @@ module "storage_accounts" {
   resources = {
     resource_groups  = module.resource_groups
     virtual_networks = module.virtual_networks
+    storage_accounts = module.storage_accounts
   }
 }
