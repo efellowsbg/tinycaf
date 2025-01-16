104e51e695055585b81cef4a2921e735b3520e65
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-16 13:22:23 +0200

    add access policies into settings

--- src/modules/_security/keyvault/_variables.tf
+++ src/modules/_security/keyvault/_variables.tf
@@ -44,13 +44,6 @@ variable "tenant_id" {
   default = {}
 }
 
-variable "access_policies" {
-  validation {
-    condition     = length(var.access_policies) <= 16
-    error_message = "A maximun of 16 access policies can be set."
-  }
-}
-
 variable "client_config" {
   description = "Client configuration object (see module README.md)."
 }
