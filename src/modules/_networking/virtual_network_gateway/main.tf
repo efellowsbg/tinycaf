ef08336175d983fe421c92707f38216b67d7d11e
Dimitar Rusev <drusev@efellows.bg>
2025-01-16 10:33:22 +0200

    Reference the subnet by vnet_ref/subnet_ref

--- src/modules/_networking/virtual_network_gateway/main.tf
+++ src/modules/_networking/virtual_network_gateway/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_virtual_network_gateway" "main" {
       name                          = ip_configuration.value.name
       private_ip_address_allocation = try(ip_configuration.value.private_ip_address_allocation, null)
       public_ip_address_id          = var.resources.public_ips[ip_configuration.value.public_ip_address_ref].id
-      subnet_id                     = var.resources.virtual_networks[ip_configuration.value.vnet_ref].subnets[ip_configuration.value.subnet_ref].id
+      subnet_id                     = var.resources.virtual_networks[split("/", ip_configuration.value.subnet_ref)[0]].subnets[split("/", ip_configuration.value.subnet_ref)[1]].id
     }
   }
 }
