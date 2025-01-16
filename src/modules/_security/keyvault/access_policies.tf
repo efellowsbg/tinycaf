68cc9faff2f9bf5f453d44a5d8e25ecceaddd301
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-16 16:52:56 +0200

    add outputs of managed identities module

--- src/modules/_security/keyvault/access_policies.tf
+++ src/modules/_security/keyvault/access_policies.tf
@@ -15,7 +15,7 @@ resource "azurerm_key_vault_access_policy" "managed_identity" {
   }
   key_vault_id = azurerm_key_vault.main.id
   tenant_id    = var.global_settings.tenant_id
-  object_id    = var.resources.managed_identities[each.value.managed_identity_ref].object_id
+  object_id    = var.resources.managed_identities[each.value.managed_identity_ref].principal_id
 
   secret_permissions = try(each.value.secret_permissions, null) == "All" ? local.all_secret_permissions : try(each.value.secret_permissions, null)
   key_permissions    = try(each.value.key_permissions, null) == "All" ? local.all_key_permissions : try(each.value.key_permissions, null)
