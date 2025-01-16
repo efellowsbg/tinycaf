5ff742cbb0386df597d7cc7b6d1ca3960e552231
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-16 13:19:07 +0200

    fix modules about access policies

--- src/modules/_security/keyvault/_variables.tf
+++ src/modules/_security/keyvault/_variables.tf
@@ -2,14 +2,6 @@ variable "global_settings" {
   description = "Global settings for tinycaf"
 }
 
-variable "tenant_id" {
-  description = "Tenant ID"
-}
-
-variable "client_config" {
-  description = "Client configuration object (see module README.md)."
-}
-
 variable "settings" {
   description = "All the configuration for this resource"
 }
@@ -19,6 +11,7 @@ variable "resources" {
     resource_groups  = map(any)
     virtual_networks = map(any)
     managed_identities = map(any)
+    keyvaults = map(any)
   })
   description = "All required resources"
 }
@@ -31,4 +24,33 @@ variable "enable_policy_update_delay" {
 
 variable "managed_identities" {
   default = {}
+}
+
+
+
+
+variable "keyvaults" {
+  default = {}
+}
+
+variable "keyvault_key" {
+  default = null
+}
+variable "keyvault_id" {
+  default = null
+}
+
+variable "tenant_id" {
+  default = {}
+}
+
+variable "access_policies" {
+  validation {
+    condition     = length(var.access_policies) <= 16
+    error_message = "A maximun of 16 access policies can be set."
+  }
+}
+
+variable "client_config" {
+  description = "Client configuration object (see module README.md)."
 }
--- src/modules/_security/keyvault/initial_policy.tf
+++ src/modules/_security/keyvault/initial_policy.tf
@@ -5,7 +5,7 @@ module "initial_policy" {
   access_policies = var.settings.creation_policies
   client_config   = var.client_config
   resources = {
-    managed_identities = var.managed_identities
+    managed_identities = module.managed_identity
   }
 }
 
@@ -15,4 +15,48 @@ resource "time_sleep" "initial_policy_delay" {
   depends_on = [module.initial_policy]
 
   create_duration = "15s"
+}
+
+
+
+module "logged_in_user" {
+  source = "../keyvault_access_policies/access_policy"
+  for_each = {
+    for key, access_policy in var.access_policies : key => access_policy
+    if key == "logged_in_user" && var.client_config.logged_user_objectId != null
+  }
+
+  keyvault_id = var.keyvault_id == null ? var.resources.keyvaults[var.keyvault_key].id : var.keyvault_id
+
+  access_policy = each.value
+  tenant_id     = var.tenant_id
+  object_id     = var.client_config.object_id
+}
+
+module "object_id" {
+  source = "../keyvault_access_policies/access_policy"
+  for_each = {
+    for key, access_policy in var.access_policies : key => access_policy
+    if try(access_policy.object_id, null) != null && var.client_config.logged_aad_app_objectId != null
+  }
+
+  keyvault_id = var.keyvault_id == null ? var.resources.keyvaults[var.keyvault_key].id : var.keyvault_id
+
+  access_policy = each.value
+  tenant_id     = try(each.value.tenant_id, var.tenant_id)
+  object_id     = each.value.object_id
+}
+
+module "managed_identity" {
+  source = "../keyvault_access_policies/access_policy"
+  for_each = {
+    for key, access_policy in var.access_policies : key => access_policy
+    if try(access_policy.managed_identity_key, null) != null
+  }
+
+  keyvault_id = var.keyvault_id == null ? var.resources.keyvaults[var.keyvault_key].id : var.keyvault_id
+
+  access_policy = each.value
+  tenant_id     = var.tenant_id
+  object_id     = var.resources.managed_identities[each.value.managed_identity_key].principal_id
 }
