3bd8526be8d412ff1beb37f968795234883789f0
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-16 14:06:09 +0200

    test folder structure

--- src/modules/_security/keyvault/initial_policy.tf
+++ src/modules/_security/keyvault/initial_policy.tf
@@ -1,10 +1,13 @@
+# Initial policy is used to address a a bootstrap condition during the launchpad deployment
 module "initial_policy" {
   source = "../keyvault_access_policies"
   count  = try(var.settings.creation_policies, null) == null ? 0 : 1
-  keyvault_id     = azurerm_key_vault.main.id
+
+  keyvault_id     = azurerm_key_vault.keyvault.id
+  access_policies = var.settings.creation_policies
   client_config   = var.client_config
   resources = {
-    managed_identities = module.managed_identity
+    managed_identities = var.managed_identities
   }
 }
 
@@ -14,51 +17,4 @@ resource "time_sleep" "initial_policy_delay" {
   depends_on = [module.initial_policy]
 
   create_duration = "15s"
-}
-
-
-
-module "logged_in_user" {
-  source = "../keyvault_access_policies/access_policy"
-  access_policies = var.settings.creation_policies
-  for_each = {
-    for key, access_policy in  var.access_policies : key => access_policy
-    if key == "logged_in_user" && var.client_config.logged_user_objectId != null
-  }
-
-  keyvault_id = var.keyvault_id == null ? var.resources.keyvaults[var.keyvault_key].id : var.keyvault_id
-
-  access_policy = each.value
-  tenant_id     = var.tenant_id
-  object_id     = var.client_config.object_id
-}
-
-module "object_id" {
-  source = "../keyvault_access_policies/access_policy"
-  access_policies = var.settings.creation_policies
-  for_each = {
-    for key, access_policy in var.access_policies : key => access_policy
-    if try(access_policy.object_id, null) != null && var.client_config.logged_aad_app_objectId != null
-  }
-
-  keyvault_id = var.keyvault_id == null ? var.resources.keyvaults[var.keyvault_key].id : var.keyvault_id
-
-  access_policy = each.value
-  tenant_id     = try(each.value.tenant_id, var.tenant_id)
-  object_id     = each.value.object_id
-}
-
-module "managed_identity" {
-  source = "../keyvault_access_policies/access_policy"
-  access_policies = var.settings.creation_policies
-  for_each = {
-    for key, access_policy in var.access_policies : key => access_policy
-    if try(access_policy.managed_identity_key, null) != null
-  }
-
-  keyvault_id = var.keyvault_id == null ? var.resources.keyvaults[var.keyvault_key].id : var.keyvault_id
-
-  access_policy = each.value
-  tenant_id     = var.tenant_id
-  object_id     = var.resources.managed_identities[each.value.managed_identity_key].principal_id
 }
