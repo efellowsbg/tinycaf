f6effae41cec03edd7ffc36f6ea7da2e3e21fedc
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-16 12:52:52 +0200

    fix client_id

--- src/_locals.tf
+++ src/_locals.tf
@@ -1,6 +1,6 @@
 locals {
   client_config = var.client_config == {} ? {
-    client_id               = data.azuread_client_config.current.client_id
+    client_id               = var.client_config.client_id
     logged_aad_app_objectId = local.object_id
     logged_user_objectId    = local.object_id
     object_id               = local.object_id
