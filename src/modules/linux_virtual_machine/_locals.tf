9ab6ad65035455865174bceb15141c9e3f4f51e8
Borislav Raynov <braynov@efellows.bg>
2025-01-17 17:58:07 +0200

    Refactor subnet id in locals

--- src/modules/linux_virtual_machine/_locals.tf
+++ src/modules/linux_virtual_machine/_locals.tf
@@ -2,15 +2,13 @@ locals {
   resource_group      = var.resources.resource_groups[var.settings.resource_group_ref]
   resource_group_name = local.resource_group.name
   location            = local.resource_group.location
-  nic_config          = var.settings.network_interfaces
-
 
   network_interface_ids = [for nics_ref, config in var.settings.network_interface_ids : config.nic_ref]
 
   subnet_id = var.resources.virtual_networks[
-    split("/", nic_config.ip_configuration.subnet_ref)[0]
+    split("/", var.settings.network_interfaces.ip_configuration.subnet_ref)[0]
     ].subnets[
-    split("/", nic_config.ip_configuration.subnet_ref)[1]
+    split("/", var.settings.network_interfaces.ip_configuration.subnet_ref)[1]
   ].id
 
   public_key = tls_private_key.main[var.settings.admin_ssh_key.public_key_ref].public_key_openssh
