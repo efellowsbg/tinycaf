88bc15d4a7ad0edd7d7fdbf633ae9295cebe48a2
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-17 14:50:27 +0200

    add reference to private dns zones in keyvault

--- src/keyvault.tf
+++ src/keyvault.tf
@@ -8,5 +8,6 @@ module "keyvaults" {
     virtual_networks   = module.virtual_networks
     resource_groups    = module.resource_groups
     managed_identities = module.managed_identities
+    private_dns_zones = module.private_dns_zones
   }
 }
