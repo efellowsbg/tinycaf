273257bf9cb8617026088e3d4ccd0f5a31c98aac
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-17 13:14:07 +0200

    TC-5 Add module for private dns zone with vnet links

---
+++ src/modules/_networking/private_dns_zone/private_dns_vnet_link.tf
@@ -0,0 +1,7 @@
+resource "azurerm_private_dns_zone_virtual_network_link" "main" {
+  for_each              = local.vnet_ids
+  name                 = "${each.value.name}-${azurerm_private_dns_zone.main.name}-link"
+  private_dns_zone_name = azurerm_private_dns_zone.main.name
+  resource_group_name = azurerm_private_dns_zone.main.resource_group_name
+  virtual_network_id    = each.value.id
+}
