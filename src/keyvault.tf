3264008b8e0d9460ee50f818b496c98abdc46256
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-18 14:58:28 +0200

    test

--- src/keyvault.tf
+++ src/keyvault.tf
@@ -13,7 +13,7 @@ module "keyvaults" {
 }
 
 module "keyvault_access_policies" {
-  source   = "./modules/keyvault_access_policy"
+  source   = "./modules/keyvault/keyvault_access_policy"
 
   for_each = {
     for key, kv in var.keyvaults : key => kv.access_policies
@@ -26,6 +26,12 @@ module "keyvault_access_policies" {
 
   # ✅ Conditionally set the object_id
   object_id = contains(keys(each.value), "logged_in_user") ? var.global_settings.object_id : var.resources.managed_identities[each.value.managed_identity_ref].principal_id
-
   global_settings = var.global_settings
+  settings        = each.value
+  resources = {
+    virtual_networks   = module.virtual_networks
+    resource_groups    = module.resource_groups
+    managed_identities = module.managed_identities
+    private_dns_zones  = module.private_dns_zones
+  }
 }
