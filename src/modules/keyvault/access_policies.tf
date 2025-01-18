9aa70e3d89ed5b3d07cdd746ab5188cd06125e5a
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-18 21:31:20 +0200

    test

--- src/modules/keyvault/access_policies.tf
+++ src/modules/keyvault/access_policies.tf
@@ -1,7 +1,7 @@
-# Initial policy is used to address a a bootstrap condition during the launchpad deployment
 module "initial_policy" {
-  source = "./keyvault_access_policy"
-  for_each = try(var.settings.access_policies, {}) != {} ? var.settings.access_policies : {}
+  source          = "./keyvault_access_policy"
+  count           = try(length(var.settings.access_policies), 0) > 0 ? 1 : 0
+
   settings        = var.settings
   keyvault_id     = azurerm_key_vault.main.id
   access_policies = var.settings.access_policies
