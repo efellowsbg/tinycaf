f364741cc31c0c0e012c18f0f538b3f19127d441
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-18 22:38:45 +0200

    test with try on managed_identity_refs

--- src/modules/keyvault/keyvault_access_policy/access_policies.tf
+++ src/modules/keyvault/keyvault_access_policy/access_policies.tf
@@ -12,7 +12,8 @@ module "logged_in_user" {
 
 module "managed_identities" {
   source = "./access_policy"
-  for_each = var.policy_name == "managed_identity" ? { for idx, ref in var.access_policies.managed_identity_refs : idx => ref } : {}
+  for_each = var.policy_name == "managed_identity" && length(try(var.access_policies.managed_identity_refs, [])) > 0 ? { for idx, ref in try(var.access_policies.managed_identity_refs, []) : idx => ref } : {}
+
   keyvault_id = var.keyvault_id
   access_policies = var.access_policies
   tenant_id     = var.global_settings.tenant_id
