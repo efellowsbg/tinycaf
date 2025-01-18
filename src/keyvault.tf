6d5d42c63b6eb73bd3f07cd137de77147111378c
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-18 15:04:44 +0200

    test access policies

--- src/keyvault.tf
+++ src/keyvault.tf
@@ -11,9 +11,8 @@ module "keyvaults" {
     private_dns_zones  = module.private_dns_zones
   }
 }
-
 module "keyvault_access_policies" {
-  source   = "./modules/keyvault/keyvault_access_policy"
+  source = "./modules/keyvault/keyvault_access_policy"
 
   for_each = {
     for key, kv in var.keyvaults : key => kv.access_policies
@@ -21,17 +20,18 @@ module "keyvault_access_policies" {
   }
 
   keyvault_id = module.keyvaults[each.key].id
-  access_policy = each.value
-  tenant_id     = var.global_settings.tenant_id
 
-  # ✅ Conditionally set the object_id
-  object_id = contains(keys(each.value), "logged_in_user") ? var.global_settings.object_id : var.resources.managed_identities[each.value.managed_identity_ref].principal_id
-  global_settings = var.global_settings
+  access_policy = each.value
+
+  tenant_id      = var.global_settings.tenant_id
   settings        = each.value
+  # ✅ Conditionally set the object_id based on the policy type
+  object_id = contains(keys(each.value), "logged_in_user") ? var.global_settings.object_id : var.resources.managed_identities[each.value.managed_identity_ref].principal_id
   resources = {
     virtual_networks   = module.virtual_networks
     resource_groups    = module.resource_groups
     managed_identities = module.managed_identities
     private_dns_zones  = module.private_dns_zones
   }
+  global_settings = var.global_settings
 }
