f9df1553cce55f2d7202eeb97d09899f5f1e39e1
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-18 18:01:37 +0200

    test

--- src/modules/keyvault/keyvault_access_policy/access_policies.tf
+++ src/modules/keyvault/keyvault_access_policy/access_policies.tf
@@ -1,9 +1,6 @@
 module "logged_in_user" {
   source = "./access_policy"
-  for_each = {
-    for key, access_policy in var.settings : key => access_policy
-    if key == "logged_in_user" && var.global_settings.object_id != null
-  }
+  for_each = var.settings
 
   keyvault_id = var.keyvault_id == null
 
