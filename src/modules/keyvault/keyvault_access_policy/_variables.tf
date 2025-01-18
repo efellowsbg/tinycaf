76f7413f0c0f57f91fe2391c89760a9ccbe00813
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-18 14:19:59 +0200

    fix global settings

--- src/modules/keyvault/keyvault_access_policy/_variables.tf
+++ src/modules/keyvault/keyvault_access_policy/_variables.tf
@@ -18,3 +18,6 @@ variable "keyvault_id" {}
 variable "tenant_id" {}
 variable "object_id" {}
 variable "access_policy" {}
+variable "global_settings" {
+  description = "Global settings for tinycaf"
+}
