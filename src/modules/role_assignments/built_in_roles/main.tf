4fe7f3e6e5dc9bf875351a84bf82ce6423ce498a
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-19 20:45:44 +0200

    test

--- src/modules/role_assignments/built_in_roles/main.tf
+++ src/modules/role_assignments/built_in_roles/main.tf
@@ -1,21 +1,22 @@
 resource "azurerm_role_assignment" "main" {
-  for_each = tomap(flatten([
+  for_each = flatten([
     for role_definition_name, resources in var.settings : [
       for resource_key, resource_details in resources : [
         for principal_type, principals in try(resource_details, {}) : [
           for principal in principals : {
-            role_definition_name = role_definition_name
-            resource_key         = resource_key
-            resource_type        = var.resource_type
-            principal_type       = principal_type
-            principal            = principal
-            unique_key           = "${role_definition_name}-${resource_key}-${principal_type}-${principal}" # Ensure keys are unique
+            key = "${role_definition_name}-${resource_key}-${principal_type}-${principal}" # Unique key
+            value = {
+              role_definition_name = role_definition_name
+              resource_key         = resource_key
+              resource_type        = var.resource_type
+              principal_type       = principal_type
+              principal            = principal
+            }
           }
         ]
       ]
     ]
-  ]))
-
+  ])
   scope = try(var.resources[each.value.resource_type][each.value.resource_key].id, null)
 
   principal_id = try(var.resources[each.value.principal_type][each.value.principal].id, null)
