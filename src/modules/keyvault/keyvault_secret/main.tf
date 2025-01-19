002b6931e70274827a0177b9c95f7bebd741845d
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-19 14:22:13 +0200

    test secret module

--- src/modules/keyvault/keyvault_secret/main.tf
+++ src/modules/keyvault/keyvault_secret/main.tf
@@ -1,18 +1,8 @@
-
-module "keyvault" {
-  source = "../keyvault"
-}
-
-
-
 resource "azurerm_key_vault_secret" "main" {
-  for_each = {
-    for key, value in try(var.settings.secrets, {}) :
-    key => value
-  }
-  name         = each.value.name
-  value        = each.value.value
-  key_vault_id = module.keyvault.id
+
+  name         = var.secrets.name
+  value        = var.secrets.value
+  key_vault_id = var.keyvault_id
 
   lifecycle {
     ignore_changes = [value]
