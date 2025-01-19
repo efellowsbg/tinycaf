905f8591c0b25009828e3dc0429573b2ae64697c
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-19 19:55:32 +0200

    Test

--- src/modules/role_assignments/built_in_roles/main.tf
+++ src/modules/role_assignments/built_in_roles/main.tf
@@ -1,7 +1,29 @@
 resource "azurerm_role_assignment" "main" {
-  for_each = var.settings.keyvaults
+  # Flatten the role assignments structure into a map
+  for_each = tomap(flatten([
+    for resource_type, roles in var.role_assignments.built_in_roles : [
+      for role_definition_name, resources in roles : [
+        for resource_key, resource_details in resources : [
+          for principal_key, principal_list in resource_details : [
+            for principal in try(principal_list, []) : {
+              resource_type       = resource_type
+              role_definition_name = role_definition_name
+              resource_key         = resource_key
+              principal_type       = principal_key
+              principal            = principal
+            }
+          ]
+        ]
+      ]
+    ]
+  ]))
 
-  scope                = var.resources.keyvaults[each.key].id
-  role_definition_name = var.settings.role_definition_name
-  principal_id         = var.resources.managed_identities[each.value.managed_identity_refs[0]].id
+  # Dynamically resolve the scope
+  scope = try(var.resources[each.value.resource_type][each.value.resource_key].id, null)
+
+  # Dynamically resolve the principal_id based on the principal type and key
+  principal_id = try(var.resources[each.value.principal_type][each.value.principal].id, null)
+
+  # Role definition
+  role_definition_name = each.value.role_definition_name
 }
--- src/modules/role_assignments/main.tf
+++ src/modules/role_assignments/main.tf
@@ -1,18 +1,12 @@
 module "built_in_roles" {
-  source          = "./built_in_roles"
+  source = "./built_in_roles"
 
-  # Flatten the structure and include parent keys in the iteration
   for_each = {
-  for idx, item in flatten([
-    for resource_type, roles in try(var.settings.built_in_roles, {}) : [
-      for role, kv_refs in roles : {
-        resource_type       = resource_type
-        role_definition_name = role
-        keyvaults           = kv_refs
-      }
-    ]
-  ]) : idx => item
-}
+    for resource_type, roles in try(var.role_assignments.built_in_roles, {}) : resource_type => {
+      resource_type = resource_type
+      roles         = roles
+    }
+  }
 
   settings        = each.value
   global_settings = var.global_settings
