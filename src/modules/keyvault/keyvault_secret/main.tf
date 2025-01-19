edeee69b10ba6b053f7c88d537d4f9f80e13e644
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-19 14:32:48 +0200

    update module secrets

--- src/modules/keyvault/keyvault_secret/main.tf
+++ src/modules/keyvault/keyvault_secret/main.tf
@@ -1,10 +1,9 @@
 resource "azurerm_key_vault_secret" "main" {
-
   name         = var.secrets.name
   value        = var.secrets.value
   key_vault_id = var.keyvault_id
 
   lifecycle {
-    ignore_changes = [value]
+    ignore_changes = var.secrets.ignore_changes ? [value] : []
   }
 }
--- src/modules/keyvault/secrets.tf
+++ src/modules/keyvault/secrets.tf
@@ -1,10 +1,12 @@
 module "secrets" {
   source          = "./keyvault_secret"
+
+  # Use for_each to iterate over the secrets map
   for_each        = try(var.settings.secrets, {})
 
   settings        = var.settings
   keyvault_id     = azurerm_key_vault.main.id
-  secrets = each.value
+  secrets         = each.value
   global_settings = var.global_settings
-  resources = var.resources
+  resources       = var.resources
 }
