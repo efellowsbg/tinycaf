447cb0e72ae59e38c27b86f5fa3886efda42d278
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-19 16:52:52 +0200

    fix max pods

--- src/modules/compute/kubernetes/kubernetes_cluster/aks.tf
+++ src/modules/compute/kubernetes/kubernetes_cluster/aks.tf
@@ -8,7 +8,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     node_count = try(var.settings.default_node_pool.node_count, 1)
     vm_size    = try(var.settings.default_node_pool.vm_size, "Standard_D3_v2")
     type = try(var.settings.default_node_pool.type, "VirtualMachineScaleSets")
-    max_pods = try(var.settings.default_node_pool.max_pods, "VirtualMachineScaleSets")
+    max_pods = try(var.settings.default_node_pool.max_pods, 120)
     vnet_subnet_id = local.vnet_subnet_id
   }
   identity {
