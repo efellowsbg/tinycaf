39a51bc620564c3848b671ec59bca83f5f6c6dd1
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-19 19:39:44 +0200

    test

--- src/modules/role_assignments/main.tf
+++ src/modules/role_assignments/main.tf
@@ -1,6 +1,17 @@
 module "built_in_roles" {
   source          = "./built_in_roles"
-  for_each        = try(var.settings.built_in_roles, {})
+
+  # Flatten the structure and include parent keys in the iteration
+  for_each = tomap(flatten([
+    for resource_type, roles in try(var.settings.built_in_roles, {}) : [
+      for role, kv_refs in roles : {
+        resource_type       = resource_type
+        role_definition_name = role
+        keyvaults           = kv_refs
+      }
+    ]
+  ]))
+
   settings        = each.value
   global_settings = var.global_settings
   resources       = var.resources
