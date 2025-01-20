8296df31393bf499cc0acb63c6713da5f825fe57
Borislav Raynov <braynov@efellows.bg>
2025-01-20 16:06:07 +0200

    Refactored locals

--- src/_locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  global_settings = merge(var.global_settings, {
-    object_id       = data.azurerm_client_config.current.object_id
-    subscription_id = data.azurerm_client_config.current.subscription_id
-    tenant_id       = data.azurerm_client_config.current.tenant_id
-    client_id       = data.azurerm_client_config.current.client_id
-  })
-}
