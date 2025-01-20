c067efae4c73ca73f128a6d0304b79b3dbd84607
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-20 16:16:23 +0200

    remove unused variable

--- src/modules/compute/kubernetes/aks.tf
+++ src/modules/compute/kubernetes/aks.tf
@@ -12,7 +12,6 @@ module "kubernetes_cluster_node_pool" {
   cluster_id = module.kubernetes_cluster.id
   all_settings = var.settings
   settings        = each.value
-  pool_value = each.value
   global_settings = var.global_settings
   resources = var.resources
 }
