bda9595e411e41b2ba06b950cef82ca26d3b5b8d
Borislav Raynov <braynov@efellows.bg>
2025-01-20 16:21:59 +0200

    Refactor locals

--- src/modules/virtual_machines/linux_virtual_machine/_locals.tf
+++ src/modules/virtual_machines/linux_virtual_machine/_locals.tf
@@ -3,10 +3,10 @@ locals {
   resource_group_name = local.resource_group.name
   location            = local.resource_group.location
 
-  network_interface_ids = [
-    for nics_ref, config in var.settings.network_interface_ids :
-    azurerm_network_interface.main[config.nic_ref].id
-  ]
+  # network_interface_ids = [
+  #   for nics_ref, config in var.settings.network_interface_ids :
+  #   azurerm_network_interface.main[config.nic_ref].id
+  # ]
 
   subnet_id = {
     for nic, config in try(var.settings.network_interfaces) :
@@ -17,7 +17,7 @@ locals {
     ].id
   }
 
-  public_key = tls_private_key.main[var.settings.admin_ssh_key.public_key_ref].public_key_openssh
+  # public_key = tls_private_key.main[var.settings.admin_ssh_key.public_key_ref].public_key_openssh
 
   tags = merge(
     var.global_settings.tags,
