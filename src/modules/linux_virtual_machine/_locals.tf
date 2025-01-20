0519fecb54e8e5f523b2e9550d55df32fffa389a
Borislav Raynov <braynov@efellows.bg>
2025-01-20 10:44:51 +0200

    Refactor public key in locals

--- src/modules/linux_virtual_machine/_locals.tf
+++ src/modules/linux_virtual_machine/_locals.tf
@@ -5,12 +5,6 @@ locals {
 
   network_interface_ids = [for nics_ref, config in var.settings.network_interface_ids : config.nic_ref]
 
-  # subnet_id = var.resources.virtual_networks[
-  #   split("/", var.settings.network_interfaces.ip_configuration.subnet_ref)[0]
-  #   ].subnets[
-  #   split("/", var.settings.network_interfaces.ip_configuration.subnet_ref)[1]
-  # ].id
-
   subnet_id = {
     for nic, config in var.settings.network_interfaces :
     nic => var.resources.virtual_networks[
@@ -20,8 +14,8 @@ locals {
     ].id
   }
 
-  public_key = tls_private_key.main[var.settings.admin_ssh_key.public_key_ref]
-  # .public_key_openssh
+  # public_key = tls_private_key.main[var.settings.admin_ssh_key.public_key_ref].public_key_openssh
+  public_key = tls_private_key.main.public_key_openssh
 
   tags = merge(
     var.global_settings.tags,
