f268801cb611bda44bd5260ed1cde3858162abf4
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-20 22:29:38 +0200

    Add support for access policies with object IDs

--- src/modules/keyvault/keyvault_access_policy/access_policies.tf
+++ src/modules/keyvault/keyvault_access_policy/access_policies.tf
@@ -21,3 +21,15 @@ module "managed_identities" {
   key_permissions = local.effective_key_permissions
   secret_permissions = local.effective_secret_permissions
 }
+
+module "object_ids" {
+  source = "./access_policy"
+  for_each = var.policy_name == "object_ids" && length(try(var.access_policies.object_ids, [])) > 0 ? { for idx, obj_id in try(var.access_policies.object_ids, []) : idx => obj_id } : {}
+
+  keyvault_id        = var.keyvault_id
+  access_policies    = var.access_policies
+  tenant_id          = var.global_settings.tenant_id
+  object_id          = each.value
+  key_permissions    = local.effective_key_permissions
+  secret_permissions = local.effective_secret_permissions
+}
