beb615a8cb88cfc9105795ffe07bd1e007a0d984
Lyudmil Ilchev <lilchev@efellows.bg>
2025-01-20 16:08:37 +0200

    Add aks module

--- src/modules/compute/kubernetes/kubernetes_cluster/_locals.tf
+++ src/modules/compute/kubernetes/kubernetes_cluster/_locals.tf
@@ -1,9 +1,12 @@
 locals {
   resource_group = var.resources.resource_groups[var.settings.resource_group_ref]
-
+  validated_network_data_plane = var.settings.network_profile.network_policy == "cilium" && var.settings.network_profile.network_data_plane != "cilium" ? error("Error: When network_policy is set to 'cilium', the network_data_plane must also be set to 'cilium'.") : var.settings.network_profile.network_data_plane
   resource_group_name = local.resource_group.name
   location            = local.resource_group.location
+  node_resource_group = var.resources.resource_groups[var.settings.node_resource_group_ref]
 
+  node_resource_group_name = local.node_resource_group.name
+  node_location            = local.node_resource_group.location
   subnet_ids = [
     for network_rule_ref, config in try(var.settings.network_rules.subnets, {}) : (
       var.resources.virtual_networks[split("/", config.subnet_ref)[0]].subnets[split("/", config.subnet_ref)[1]].id
@@ -18,4 +21,6 @@ locals {
     var.global_settings.inherit_resource_group_tags ? local.resource_group.tags : {},
     try(var.settings.tags, {})
   )
+  validated_pod_cidr = var.settings.network_profile.network_plugin == "azure" && var.settings.network_profile.pod_cidr != null && var.settings.network_profile.network_plugin_mode != "overlay" ? error("Error: When network_plugin is 'azure', pod_cidr must not be set unless network_plugin_mode is 'overlay'.") : var.settings.network_profile.pod_cidr
+
 }
