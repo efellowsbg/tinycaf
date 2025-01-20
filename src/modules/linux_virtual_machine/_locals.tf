9c2234c833b69917b2bf033b40afd0f6201cfcbb
Borislav Raynov <braynov@efellows.bg>
2025-01-20 11:24:02 +0200

    Refactor public key in locals

--- src/modules/linux_virtual_machine/_locals.tf
+++ src/modules/linux_virtual_machine/_locals.tf
@@ -19,7 +19,7 @@ locals {
     ].id
   }
 
-  public_key = tls_private_key.main[var.settings.admin_ssh_key.main[public_key_ref]].public_key_openssh
+  public_key = tls_private_key.main[var.settings.admin_ssh_key.public_key_ref].public_key_openssh
   # public_key = tls_private_key.main.public_key_openssh
 
   tags = merge(
