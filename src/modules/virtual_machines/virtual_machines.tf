ea096206938b8341d8d8201b7a87d8def401ece1
Borislav Raynov <braynov@efellows.bg>
2025-01-21 13:41:24 +0200

    Refactor code

--- src/modules/virtual_machines/virtual_machines.tf
+++ src/modules/virtual_machines/virtual_machines.tf
@@ -10,11 +10,12 @@ module "linux_virtual_machine" {
 }
 
 module "network_interface" {
-  source   = "./network_interface"
-  for_each = var.settings.network_interfaces
-
-  settings        = each.value
+  source          = "./network_interface"
+  for_each        = var.settings.network_interfaces
   global_settings = var.global_settings
+  all_settings    = var.settings
+  settings        = each.value
+
 
   resources = var.resources
 }
