551bac4b4a301054e556d9491482f0d71716e8cd
Borislav Raynov <braynov@efellows.bg>
2025-01-21 16:37:40 +0200

    Remove comments

--- src/virtual_machines.tf
+++ src/virtual_machines.tf
@@ -1,6 +1,5 @@
 module "virtual_machines" {
   source = "./modules/virtual_machines"
-  # for_each = var.virtual_machines
 
   settings        = var.virtual_machines
   global_settings = var.global_settings
