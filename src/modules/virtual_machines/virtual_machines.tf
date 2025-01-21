4fe1ccdf5a9f6dee7347d6ef30ed3f0c151f429d
Borislav Raynov <braynov@efellows.bg>
2025-01-21 16:19:18 +0200

    Add windows virtual machine module

--- src/modules/virtual_machines/virtual_machines.tf
+++ src/modules/virtual_machines/virtual_machines.tf
@@ -1,21 +1,21 @@
 module "linux_virtual_machine" {
   source = "./linux_virtual_machine"
 
-  settings        = var.settings
+  for_each = { for key, vm in var.settings : key => vm if vm.type == "linux" }
+
+  settings        = each.value
   global_settings = var.global_settings
 
   resources = var.resources
 }
 
-# module "windows_virtual_machine" {
-#   source   = "./modules/linux_virtual_machine"
-#   for_each = var.linux_virtual_machine
+module "windows_virtual_machine" {
+  source = "./windows_virtual_machine"
 
-#   settings        = each.value
-#   global_settings = var.global_settings
+  for_each = { for key, vm in var.settings : key => vm if vm.type == "windows" }
 
-#   resources = {
-#     resource_groups  = module.resource_groups
-#     virtual_networks = module.virtual_networks
-#   }
-# }
+  settings        = each.value
+  global_settings = var.global_settings
+
+  resources = var.resources
+}
---
+++ src/modules/virtual_machines/windows_virtual_machine/windows_virtual_macine.tf
@@ -0,0 +1,23 @@
+resource "azurerm_windows_virtual_machine" "main" {
+  name                  = var.settings.name
+  resource_group_name   = local.resource_group_name
+  location              = local.location
+  admin_username        = var.settings.admin_username
+  admin_password        = var.settings.admin_password
+  size                  = var.settings.size
+  network_interface_ids = try(local.network_interface_ids, null)
+
+  tags = local.tags
+
+  os_disk {
+    caching              = var.settings.os_disk.caching
+    storage_account_type = var.settings.os_disk.storage_account_type
+  }
+
+  source_image_reference {
+    publisher = var.settings.source_image_reference.publisher
+    offer     = var.settings.source_image_reference.offer
+    sku       = var.settings.source_image_reference.sku
+    version   = var.settings.source_image_reference.version
+  }
+}
