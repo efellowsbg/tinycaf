7a55a441dadd50e991f7a37bffc3755ca568b18c
Borislav Raynov <braynov@efellows.bg>
2025-01-21 15:17:54 +0200

    Refactor code

--- src/modules/virtual_machines/linux_virtual_machine/_outputs.tf
+++ src/modules/virtual_machines/linux_virtual_machine/_outputs.tf
@@ -10,6 +10,6 @@ output "public_ip_addresses" {
   value = azurerm_linux_virtual_machine.main.public_ip_addresses
 }
 
-output "nic_ids" {
-  value = module.network_interface.id
-}
+# output "nic_ids" {
+#   value = module.network_interface.id
+# }
