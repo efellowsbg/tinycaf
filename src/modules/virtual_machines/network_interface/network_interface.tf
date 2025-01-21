1fc003d00742b3747c098ed4febd981f70fadb3d
Borislav Raynov <braynov@efellows.bg>
2025-01-21 12:09:56 +0200

    Refactor resource

--- src/modules/virtual_machines/network_interface/network_interface.tf
+++ src/modules/virtual_machines/network_interface/network_interface.tf
@@ -1,10 +1,10 @@
 resource "azurerm_network_interface" "main" {
-  name                = each.value.name
+  name                = var.settings.network_interfaces.name
   resource_group_name = local.resource_group_name
   location            = local.location
 
   ip_configuration {
-    name                          = each.value.ip_configuration.name
+    name                          = var.settings.network_interfaces.ip_configuration.name
     subnet_id                     = local.subnet_id
     private_ip_address_allocation = try(var.settings.ip_configuration.private_ip_address_allocation, "Dynamic")
   }
