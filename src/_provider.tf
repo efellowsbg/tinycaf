ebf68215e0c7093c1d7f00ffe6331758aba4e106
Borislav Raynov <braynov@efellows.bg>
2025-01-22 13:57:57 +0200

    Refactor provider

--- src/_provider.tf
+++ src/_provider.tf
@@ -2,12 +2,21 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.14.0"
+      version = "~>4.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
+  skip_provider_registration = true
+  features {
+    key_vault {
+      recover_soft_deleted_key_vaults = true
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  subscription_id = "cf1839da-5f7c-4fb5-8a06-f6cb6d0d53df"
 }
+data "azurerm_client_config" "current" {}
