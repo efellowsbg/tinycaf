7165346f97c18c51c5d1d5c5880e768da14d9801
Borislav Raynov <braynov@efellows.bg>
2025-01-22 13:51:38 +0200

    Refactor secret

--- src/modules/keyvault/keyvault_secret/main.tf
+++ src/modules/keyvault/keyvault_secret/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_key_vault_secret" "main" {
   name         = var.secrets.name
-  value        = var.secrets.value
+  value        = tls_private_key.main.private_key_pem
   key_vault_id = var.keyvault_id
 
   lifecycle {
