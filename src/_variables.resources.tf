0ca1100096f6ec5bd4364428cb8c4f128e0998fe
Borislav Raynov <braynov@efellows.bg>
2025-01-23 17:17:40 +0200

    Remove unnecessary resources

--- src/_variables.resources.tf
+++ src/_variables.resources.tf
@@ -1,16 +1,6 @@
 variable "resource_groups" { default = {} }
 
-variable "managed_identities" { default = {} }
-
 variable "virtual_networks" { default = {} }
-
-variable "vnet_peerings" { default = {} }
-
-variable "local_network_gateways" { default = {} }
-
-variable "virtual_network_gateways" { default = {} }
-
-variable "public_ips" { default = {} }
 
 variable "storage_accounts" { default = {} }
 
--- src/modules/managed_identity/main.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_user_assigned_identity" "main" {
-  name                = var.settings.name
-  resource_group_name = local.resource_group_name
-  location            = local.location
-}
