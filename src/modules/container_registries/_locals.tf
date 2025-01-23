5032e350edf9aca52cb9f92f1f706d3cdf1fba60
Borislav Raynov <braynov@efellows.bg>
2025-01-23 13:43:36 +0200

    Refactor locals

--- src/modules/container_registries/_locals.tf
+++ src/modules/container_registries/_locals.tf
@@ -4,8 +4,8 @@ locals {
   location            = local.resource_group.location
 
   dns_zone_group       = var.resources.private_dns_zones[var.settings.private_endpoint.private_dns_zone_group_ref]
-  dns_zone_group_name  = dns_zone_group.name
-  private_dns_zone_ids = [dns_zone_group.name.id]
+  dns_zone_group_name  = local.dns_zone_group.name
+  private_dns_zone_ids = [local.dns_zone_group.id]
 
   subnet_id = var.resources.virtual_networks[
     split("/", var.settings.private_endpoint.subnet_ref)[0]
