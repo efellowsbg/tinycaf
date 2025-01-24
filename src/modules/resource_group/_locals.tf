4c8ce4f20cc56c7374f822de35e407f8c810553a
Borislav Raynov <braynov@efellows.bg>
2025-01-24 10:08:33 +0200

    Refactor tags

---
+++ src/modules/resource_group/_locals.tf
@@ -0,0 +1,7 @@
+locals {
+  tags = merge(
+    var.global_settings.tags,
+    var.global_settings.inherit_resource_group_tags ? local.resource_group.tags : {},
+    try(var.settings.tags, {})
+  )
+}
