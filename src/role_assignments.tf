2091db80daadcc019374532bbddd069541c73a8a
lyudmililchev92 <lyudmil.ilchev@teladochealth.com>
2025-01-27 10:56:54 +0200

    fix module for aks

--- src/role_assignments.tf
+++ src/role_assignments.tf
@@ -9,6 +9,6 @@ module "role_assignments" {
     resource_groups   = module.resource_groups
     keyvaults         = module.keyvaults
     managed_identities = module.managed_identities
-    kubernetes_clusters = module.kubernetes_clusters.kubernetes_cluster
+    kubernetes_clusters = module.kubernetes_clusters
   }
 }
