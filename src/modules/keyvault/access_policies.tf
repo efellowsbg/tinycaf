e3b1ecd73b4f74c5187541b3b3476011c363343f
Borislav Raynov <braynov@efellows.bg>
2025-01-27 15:45:07 +0200

    Refactor code

--- src/modules/keyvault/access_policies.tf
+++ src/modules/keyvault/access_policies.tf
@@ -2,9 +2,11 @@ module "initial_policy" {
   source          = "./keyvault_access_policy"
   for_each        = try(var.settings.access_policies, {})
   settings        = var.settings
+  global_settings = var.global_settings
+
   keyvault_id     = azurerm_key_vault.main.id
   access_policies = each.value
-  policy_name =     each.key
-  global_settings = var.global_settings
+  policy_name     = each.key
+
   resources = var.resources
 }
