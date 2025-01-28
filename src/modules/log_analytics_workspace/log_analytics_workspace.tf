6c74c35b9d3be62ac6125c02d3ae2f388ba77d59
Borislav Raynov <braynov@efellows.bg>
2025-01-28 12:15:09 +0200

    Change default values in optional blocks

--- src/modules/log_analytics_workspace/log_analytics_workspace.tf
+++ src/modules/log_analytics_workspace/log_analytics_workspace.tf
@@ -4,19 +4,23 @@ resource "azurerm_log_analytics_workspace" "main" {
   location            = local.location
   tags                = local.tags
 
-  retention_in_days                       = try(var.settings.retention_in_days, null)
   sku                                     = try(var.settings.sku, "PerGB2018")
-  allow_resource_only_permissions         = try(var.settings.allow_resource_only_permissions, true)
-  local_authentication_disabled           = try(var.settings.local_authentication_disabled, false)
-  cmk_for_query_forced                    = try(var.settings.cmk_for_query_forced, null)
   internet_ingestion_enabled              = try(var.settings.internet_ingestion_enabled, true)
+  allow_resource_only_permissions         = try(var.settings.allow_resource_only_permissions, true)
   internet_query_enabled                  = try(var.settings.internet_query_enabled, true)
+  cmk_for_query_forced                    = try(var.settings.cmk_for_query_forced, null)
   data_collection_rule_id                 = try(var.settings.data_collection_rule_id, null)
   immediate_data_purge_on_30_days_enabled = try(var.settings.immediate_data_purge_on_30_days_enabled, null)
+  local_authentication_disabled           = try(var.settings.local_authentication_disabled, false)
+
+  retention_in_days = try(
+    var.settings.retention_in_days,
+    var.settings.sku == "Free" ? "7" : "30"
+  )
 
   daily_quota_gb = try(
     var.settings.daily_quota_gb,
-    var.settings.sku == "Free" ? "0.5" : "-1"
+    var.settings.sku == "Free" ? "0.5" : null
   )
 
   reservation_capacity_in_gb_per_day = try(
