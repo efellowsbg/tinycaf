562681ae6ef3fe2637b103096641b6e1e9a05657
Borislav Raynov <braynov@efellows.bg>
2025-01-28 13:23:18 +0200

    Change default values in resource

--- src/modules/log_analytics_workspace/log_analytics_workspace.tf
+++ src/modules/log_analytics_workspace/log_analytics_workspace.tf
@@ -5,24 +5,15 @@ resource "azurerm_log_analytics_workspace" "main" {
   tags                = local.tags
 
   sku                                     = try(var.settings.sku, "PerGB2018")
+  retention_in_days                       = try(var.settings.retention_in_days, "30")
   internet_ingestion_enabled              = try(var.settings.internet_ingestion_enabled, true)
   allow_resource_only_permissions         = try(var.settings.allow_resource_only_permissions, true)
   internet_query_enabled                  = try(var.settings.internet_query_enabled, true)
   cmk_for_query_forced                    = try(var.settings.cmk_for_query_forced, null)
   data_collection_rule_id                 = try(var.settings.data_collection_rule_id, null)
   immediate_data_purge_on_30_days_enabled = try(var.settings.immediate_data_purge_on_30_days_enabled, null)
+  daily_quota_gb                          = try(var.settings.daily_quota_gb, null)
   local_authentication_disabled           = try(var.settings.local_authentication_disabled, false)
-
-  retention_in_days = try(
-    var.settings.retention_in_days,
-    var.settings.sku == "Free" ? "7" : "30"
-  )
-
-  daily_quota_gb = try(
-    var.settings.daily_quota_gb,
-    var.settings.sku == "Free" ? "0.5" : null,
-    null
-  )
 
   reservation_capacity_in_gb_per_day = try(
     var.settings.sku == "CapacityReservation" ?
