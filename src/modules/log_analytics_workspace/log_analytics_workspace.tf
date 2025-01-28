31d7f74281e3eae4b78576b3931c971ca0acadd8
Borislav Raynov <braynov@efellows.bg>
2025-01-28 13:32:48 +0200

    Fix references in the dynamic block.

--- src/modules/log_analytics_workspace/log_analytics_workspace.tf
+++ src/modules/log_analytics_workspace/log_analytics_workspace.tf
@@ -25,10 +25,10 @@ resource "azurerm_log_analytics_workspace" "main" {
     for_each = try(var.settings.identities[*], {})
 
     content {
-      type = try(identity.type, null)
+      type = try(identity.value.type, null)
       identity_ids = (
         identity.type == "UserAssigned" ?
-        (try(identity.identity_ids, null) != null ? identity.identity_ids : null) :
+        (try(identity.value.identity_ids, null) != null ? identity.value.identity_ids : null) :
         null
       )
     }
