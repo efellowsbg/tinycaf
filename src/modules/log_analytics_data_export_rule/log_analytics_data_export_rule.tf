c3a8a7b70df6c791dcc9a993c7d7a1a40c3bc480
Borislav Raynov <braynov@efellows.bg>
2025-01-29 10:41:36 +0200

    Add dynamic block for timeouts

--- src/modules/log_analytics_data_export_rule/log_analytics_data_export_rule.tf
+++ src/modules/log_analytics_data_export_rule/log_analytics_data_export_rule.tf
@@ -4,5 +4,17 @@ resource "azurerm_log_analytics_data_export_rule" "main" {
   workspace_resource_id   = local.workspace_resource_id
   destination_resource_id = local.destination_resource_id
   table_names             = var.settings.table_names
-  enabled                 = var.settings.enabled
+
+  enabled = try(var.settings.enabled, false)
+
+  dynamic "timeouts" {
+    for_each = try(var.settings.timeouts[*], {})
+
+    content {
+      read   = try(timeouts.value.read, null)
+      create = try(timeouts.value.create, null)
+      update = try(timeouts.value.update, null)
+      delete = try(timeouts.value.delete, null)
+    }
+  }
 }
