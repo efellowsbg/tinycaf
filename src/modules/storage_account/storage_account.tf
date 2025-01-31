204c00c6d396b6172e107dbdb67fb97aa0fbc55b
lyudmililchev92 <lyudmil.ilchev@teladochealth.com>
2025-01-31 17:24:43 +0200

    add setting for storage account - nested items to be public

--- src/modules/storage_account/storage_account.tf
+++ src/modules/storage_account/storage_account.tf
@@ -7,6 +7,7 @@ resource "azurerm_storage_account" "main" {
   account_kind             = try(var.settings.account_kind, "StorageV2")
   account_tier             = try(var.settings.account_tier, "Standard")
   account_replication_type = var.settings.account_replication_type
+  allow_nested_items_to_be_public = try(var.settings.allow_nested_items_to_be_public, null)
 
   cross_tenant_replication_enabled  = try(var.settings.cross_tenant_replication_enabled, null)
   large_file_share_enabled          = try(var.settings.large_file_share_enabled, null)
