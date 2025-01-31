cfc2de06a1ca5d1d99936d2c6b94c39be9bf0fa4
lyudmililchev92 <lyudmil.ilchev@teladochealth.com>
2025-01-31 17:14:31 +0200

    Add private endpoint for storage account

---
+++ src/modules/storage_account/private_endpoint.tf
@@ -0,0 +1,13 @@
+module "storage_account_endpoint" {
+  source = "./private_endpoint"
+
+  count = try(var.settings.private_endpoint != null, false) ? 1 : 0
+
+  settings        = var.settings
+  storage_acccount_id     = azurerm_storage_account.main.id
+  subnet_ref      = var.settings.private_endpoint.subnet_ref
+  dns_zones_ref   = var.settings.private_endpoint.dns_zones_ref
+  global_settings = var.global_settings
+  resources       = var.resources
+}
+
