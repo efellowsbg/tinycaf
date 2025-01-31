8c64bdde495cc324d0cc82b477d83436099900a2
Borislav Raynov <braynov@efellows.bg>
2025-01-31 15:53:16 +0200

    Fix output typo

--- src/modules/key_vault_key/_outputs.tf
+++ src/modules/key_vault_key/_outputs.tf
@@ -6,6 +6,6 @@ output "resource_versionless_id" {
   value = azurerm_key_vault_key.main.resource_versionless_id
 }
 
-output "id" {
+output "versionless_id" {
   value = azurerm_key_vault_key.main.versionless_id
 }
