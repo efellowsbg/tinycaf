d3f3c08adad05d28d3bae6839526b5da618ca809
Lyudmil Ilchev <lilchev@efellows.bg>
2025-02-01 12:50:15 +0200

    Fix policies folder path

--- src/policies.tf
+++ src/policies.tf
@@ -1,5 +1,5 @@
 module "policy_definitions" {
-  source = "../modules/policies/policy_definition"
+  source = "./modules/policies/policy_definition"
 
   policy_definitions = jsondecode(file("${path.module}/main_subscription_policies.json")).landing-zones.policy_definitions
   definitions_path  = "${path.module}/policies"
