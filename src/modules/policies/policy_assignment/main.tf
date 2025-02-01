6e9a1be1e401f97565f14ef3bf3c49817e33e2e3
Lyudmil Ilchev <lilchev@efellows.bg>
2025-02-01 14:49:29 +0200

    add fix for current_scopre_resource_id

--- src/modules/policies/policy_assignment/main.tf
+++ src/modules/policies/policy_assignment/main.tf
@@ -26,8 +26,18 @@ resource "azurerm_resource_policy_assignment" "assignment" {
   name                 = each.key
   display_name         = try(jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["displayName"], "")
   description          = try(jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["description"], "")
-  policy_definition_id = try(jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["policyDefinitionId"], "")
-  resource_id               = try(jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["scope"], "")
+   # Dynamically set Subscription Scope
+  policy_definition_id = replace(
+    try(jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["policyDefinitionId"], ""),
+    "${current_scope_resource_id}",
+    "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  )
+
+  resource_id = replace(
+    try(jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["scope"], ""),
+    "${current_scope_resource_id}",
+    "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  )
   location            = try(jsondecode(file("${var.assignments_folder}/${each.value}"))["location"], "")
   parameters = jsonencode(try(jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["parameters"], {}))
 
