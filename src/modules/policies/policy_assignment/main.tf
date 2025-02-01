6b482bceacf61fced0f8d39afa7003a476632e78
Lyudmil Ilchev <lilchev@efellows.bg>
2025-02-01 15:17:27 +0200

    test with escape characters

--- src/modules/policies/policy_assignment/main.tf
+++ src/modules/policies/policy_assignment/main.tf
@@ -30,22 +30,22 @@ resource "azurerm_resource_policy_assignment" "main" {
   description          = try(jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["description"], "")
 
   policy_definition_id = replace(
-    try(
-      jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["policyDefinitionId"],
-      ""
-    ),
-    "${current_scope_resource_id}",
-    "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
-  )
+  try(
+    jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["policyDefinitionId"],
+    ""
+  ),
+  "\\${current_scope_resource_id}", # Correct escaping for Terraform
+  "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+)
 
   resource_id = replace(
-    try(
-      jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["scope"],
-      ""
-    ),
-    "${current_scope_resource_id}",
-    "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
-  )
+  try(
+    jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["scope"],
+    ""
+  ),
+  "\\${current_scope_resource_id}", # Correct escaping for Terraform
+  "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+)
   location            = try(jsondecode(file("${var.assignments_folder}/${each.value}"))["location"], "")
   identity { type = try(jsondecode(file("${var.assignments_folder}/${each.value}"))["identity"]["type"], "SystemAssigned") }
   parameters = jsonencode(try(jsondecode(file("${var.assignments_folder}/${each.value}"))["properties"]["parameters"], {}))
