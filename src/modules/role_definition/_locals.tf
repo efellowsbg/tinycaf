72442c544e8929d070252da15224c41bb7381168
Borislav Raynov <braynov@efellows.bg>
2025-02-03 12:00:57 +0200

    Handle getting id not raising error

--- src/modules/role_definition/_locals.tf
+++ src/modules/role_definition/_locals.tf
@@ -1,8 +1,7 @@
 locals {
-  # scope_resource = var.resources.resource_group_ref[var.settings.scoper_ref]
-  scope_id = local.scope_resource.id
-
   scope_resource = [for module_ref, module in var.resources :
     try(module[var.settings.scoper_ref], null)
   ]
+
+  scope_id = try(local.scope_resource.id, null)
 }
