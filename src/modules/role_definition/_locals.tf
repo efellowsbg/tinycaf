a2a6b95c80b70fb28f567d901c401c40f0c4a5ed
Borislav Raynov <braynov@efellows.bg>
2025-02-03 12:30:48 +0200

    Remove comments

--- src/modules/role_definition/_locals.tf
+++ src/modules/role_definition/_locals.tf
@@ -1,9 +1,5 @@
 locals {
-  # resource_group = var.resources.resource_groups[var.settings.resource_group_ref]
-
   scope_id = try([for module_ref, module in var.resources :
     try(module[var.settings.scope_ref].id, null)
   ][0], null)
-
-  # scope_id = try(local.scope_resource.id, null)
 }
