398204ae604d7015be857959c7e04ed0e0fb0507
Borislav Raynov <braynov@efellows.bg>
2025-02-03 11:30:30 +0200

    Remove resources for role definitions

--- src/modules/role_definition/_locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  # scope_resource = var.resources.resource_group_ref[var.settings.scoper_ref]
-  scope_id = local.scope_resource.id
-
-  scope_resource = [for module_ref, module in var.resources :
-    try(module[var.settings.scoper_ref], null)
-  ]
-}
