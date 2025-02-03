c2f897323ae7be4197ea7c66604fc746c0b825d4
Borislav Raynov <braynov@efellows.bg>
2025-02-03 13:37:36 +0200

    Set the definition to subscription level

--- src/modules/role_definition/_locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  scope_id = try([for module_ref, module in var.resources :
-    try(module[var.settings.scope_ref].id, null)
-  ][0], null)
-}
--- src/modules/role_definition/role_definition.tf
+++ src/modules/role_definition/role_definition.tf
@@ -1,6 +1,6 @@
 resource "azurerm_role_definition" "main" {
   name  = var.settings.name
-  scope = try(local.scope_id, var.settings.scope_ref)
+  scope = var.global_settings.subscription_id
 
   assignable_scopes  = try(var.settings.assignable_scopes, null)
   role_definition_id = try(var.settings.role_definition_id, null)
