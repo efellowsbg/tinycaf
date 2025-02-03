80ae38a9afc126cd9bcfadb10bbc3e73af8e20f1
Borislav Raynov <braynov@efellows.bg>
2025-02-03 12:09:47 +0200

    Test with different module referenci ni locals

--- src/modules/role_definition/_locals.tf
+++ src/modules/role_definition/_locals.tf
@@ -1,7 +1,7 @@
 locals {
-  scope_resource = try([for module_ref, module in var.resources :
-    try(module[var.settings.scoper_ref], null)
+  scope_id = try([for module_ref, module in var.resources :
+    try(module_ref[var.settings.scoper_ref].id, null)
   ][0], null)
 
-  scope_id = try(local.scope_resource.id, null)
+  # scope_id = try(local.scope_resource.id, null)
 }
