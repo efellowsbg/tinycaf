28002ed7fe3eb5e67278cc7d3430d8f167db2a13
Borislav Raynov <braynov@efellows.bg>
2025-02-04 17:27:14 +0200

    Refactor networking module

--- src/modules/_networking/virtual_network_gateway/main.tf
+++ src/modules/_networking/virtual_network_gateway/main.tf
@@ -4,12 +4,11 @@ resource "azurerm_virtual_network_gateway" "main" {
   location            = local.location
   tags                = local.tags
 
-  sku  = try(var.settings.sku, "VpnGw1")
-  type = try(var.settings.type, "Vpn")
-
+  sku           = try(var.settings.sku, "VpnGw1")
+  type          = try(var.settings.type, "Vpn")
+  active_active = try(var.settings.active_active, false)
   generation    = try(var.settings.generation, null)
   vpn_type      = try(var.settings.vpn_type, null)
-  active_active = try(var.settings.active_active, false)
   enable_bgp    = try(var.settings.enable_bgp, null)
 
   dynamic "ip_configuration" {
