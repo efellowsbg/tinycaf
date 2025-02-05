71d3449de465ed4cfc866587121615b0587863e3
Borislav Raynov <braynov@efellows.bg>
2025-02-05 11:37:34 +0200

    Add for each in nsg module

--- src/network_security_groups.tf
+++ src/network_security_groups.tf
@@ -1,7 +1,8 @@
 module "network_security_groups" {
-  source = "./modules/network_security_group"
+  source   = "./modules/network_security_group"
+  for_each = var.network_security_groups
 
-  settings        = var.network_security_groups
+  settings        = each.value
   global_settings = local.global_settings
 
   resources = {
