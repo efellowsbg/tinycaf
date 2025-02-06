9bd1613a830e6a2fe3e00a5724eddb465c9e6941
Borislav Raynov <braynov@efellows.bg>
2025-02-06 16:46:13 +0200

    Remove tags from locals

--- src/modules/_networking/network_security_group_association/_locals.tf
+++ src/modules/_networking/network_security_group_association/_locals.tf
@@ -5,10 +5,4 @@ locals {
   )
 
   network_security_group_id = try(var.resources.network_security_groups[var.settings.network_security_group_ref].id, var.settings.resource_ref)
-
-  tags = merge(
-    var.global_settings.tags,
-    var.global_settings.inherit_resource_group_tags ? local.resource_group.tags : {},
-    try(var.settings.tags, {})
-  )
 }
