6b673a014bae6e6a7c77e9127e8640aab4b444e9
Borislav Raynov <braynov@efellows.bg>
2025-02-07 16:45:17 +0200

    Import data

--- src/azuread_applications.tf
+++ src/azuread_applications.tf
@@ -1,3 +1,5 @@
+data "azurerm_client_config" "current" {}
+
 module "azuread_applications" {
   source   = "./modules/azuread_application"
   for_each = var.azuread_applications
@@ -5,7 +7,5 @@ module "azuread_applications" {
   settings        = each.value
   global_settings = local.global_settings
 
-  resources = {
-    resource_groups = module.resource_groups
-  }
+  resources = {}
 }
