ef61df4ddfbee66f96dcc6a9882817d7b6f39085
Borislav Raynov <braynov@efellows.bg>
2025-02-11 10:58:36 +0200

    Add ouputs

--- src/modules/azuread_service_principal/_outputs.tf
+++ src/modules/azuread_service_principal/_outputs.tf
@@ -1,3 +1,23 @@
-# output "key_id" {
-#   value = azuread_application.main.key_id
-# }
+output "object_id" {
+  value = azuread_service_principal.main.object_id
+}
+
+output "app_role_ids" {
+  value = azuread_service_principal.main.app_role_ids
+}
+
+output "app_roles" {
+  value = azuread_service_principal.main.app_roles
+}
+
+output "display_name" {
+  value = azuread_service_principal.main.display_name
+}
+
+output "oauth2_permission_scope_ids" {
+  value = azuread_service_principal.main.oauth2_permission_scope_ids
+}
+
+output "type" {
+  value = azuread_service_principal.main.type
+}
--- src/modules/azuread_service_principal/azuread_service_principal.tf
+++ src/modules/azuread_service_principal/azuread_service_principal.tf
@@ -2,7 +2,7 @@ resource "azuread_service_principal" "main" {
   client_id = local.client_id
 
   # Tags conflicts with feature_tags block, so comment or uncomment respoectivley
-  tags = local.tags
+  tags = toset(local.tags)
 
   owners                        = try(toset(var.settings.owners), [var.global_settings.object_id])
   app_role_assignment_required  = try(var.settings.app_role_assignment_required, null)
