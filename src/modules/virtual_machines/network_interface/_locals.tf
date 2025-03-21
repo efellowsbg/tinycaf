e014242241fa4ccac601d9f8a4ff2a7155188ad1
Borislav Raynov <braynov@efellows.bg>
2025-03-21 11:27:46 +0200

    Fix public ip ref

--- src/modules/virtual_machines/network_interface/_locals.tf
+++ src/modules/virtual_machines/network_interface/_locals.tf
@@ -3,7 +3,7 @@ locals {
   resource_group_name = local.resource_group.name
   location            = local.resource_group.location
 
-  public_ip_address_id = var.resources.public_ips[var.settings.public_ip_address_ref].id
+  public_ip_address_id = var.resources.public_ips[each.value.ip_configuration.public_ip_address_ref].id
 
   tags = merge(
     var.global_settings.tags,
