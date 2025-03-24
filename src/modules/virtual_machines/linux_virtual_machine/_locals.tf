787f49ac0de626502481f7951a9cea8a4fe7b062
Borislav Raynov <braynov@efellows.bg>
2025-03-24 10:09:37 +0200

    Add index of vm_key property

--- src/modules/virtual_machines/linux_virtual_machine/_locals.tf
+++ src/modules/virtual_machines/linux_virtual_machine/_locals.tf
@@ -7,7 +7,7 @@ locals {
 
   key_vault_id = var.resources.keyvaults[var.settings.keyvault_ref].id
 
-  vm_key      = tls_private_key.main[var.settings.admin_ssh_key.public_key_ref]
+  vm_key      = tls_private_key.main[var.settings.admin_ssh_key[1].public_key_ref]
   private_key = local.vm_key.private_key_pem
   public_key  = local.vm_key.public_key_openssh
 
