ab591a151bf2bd299fab980df3ea7d3de2758536
Ivan Kuzmanov <ikuzmanov@efellows.bg>
2025-03-26 16:47:45 +0200

    Refactor logic

--- src/modules/logic_app/standard/main.tf
+++ src/modules/logic_app/standard/main.tf
@@ -32,31 +32,31 @@ resource "azurerm_logic_app_standard" "main" {
   dynamic "site_config" {
     for_each = can(var.settings.site_config) ? [1] : []
     content {
-      always_on                        = try(site_config.value.always_on, null)
-      app_scale_limit                  = try(site_config.value.app_scale_limit, null)
-      auto_swap_slot_name              = try(site_config.value.auto_swap_slot_name, null)
-      dotnet_framework_version         = try(site_config.value.dotnet_framework_version, null)
-      elastic_instance_minimum         = try(site_config.value.elastic_instance_minimum, null)
-      ftps_state                       = try(site_config.value.ftps_state, null)
-      health_check_path                = try(site_config.value.health_check_path, null)
-      http2_enabled                    = try(site_config.value.http2_enabled, null)
-      ip_restriction                   = try(site_config.value.ip_restriction, null)
-      scm_ip_restriction               = try(site_config.value.scm_ip_restriction, null)
-      scm_use_main_ip_restriction      = try(site_config.value.scm_use_main_ip_restriction, null)
-      scm_min_tls_version              = try(site_config.value.scm_min_tls_version, null)
-      scm_type                         = try(site_config.value.scm_type, null)
-      linux_fx_version                 = try(site_config.value.linux_fx_version, null)
-      min_tls_version                  = try(site_config.value.min_tls_version, null)
-      pre_warmed_instance_count        = try(site_config.value.pre_warmed_instance_count, null)
-      runtime_scale_monitoring_enabled = try(site_config.value.runtime_scale_monitoring_enabled, null)
-      use_32_bit_worker_process        = try(site_config.value.use_32_bit_worker_process, null)
-      vnet_route_all_enabled           = try(site_config.value.vnet_route_all_enabled, null)
-      websockets_enabled               = try(site_config.value.websockets_enabled, null)
+      always_on                        = try(var.settings.site_config.always_on, null)
+      app_scale_limit                  = try(var.settings.site_config.app_scale_limit, null)
+      auto_swap_slot_name              = try(var.settings.site_config.auto_swap_slot_name, null)
+      dotnet_framework_version         = try(var.settings.site_config.dotnet_framework_version, null)
+      elastic_instance_minimum         = try(var.settings.site_config.elastic_instance_minimum, null)
+      ftps_state                       = try(var.settings.site_config.ftps_state, null)
+      health_check_path                = try(var.settings.site_config.health_check_path, null)
+      http2_enabled                    = try(var.settings.site_config.http2_enabled, null)
+      ip_restriction                   = try(var.settings.site_config.ip_restriction, null)
+      scm_ip_restriction               = try(var.settings.site_config.scm_ip_restriction, null)
+      scm_use_main_ip_restriction      = try(var.settings.site_config.scm_use_main_ip_restriction, null)
+      scm_min_tls_version              = try(var.settings.site_config.scm_min_tls_version, null)
+      scm_type                         = try(var.settings.site_config.scm_type, null)
+      linux_fx_version                 = try(var.settings.site_config.linux_fx_version, null)
+      min_tls_version                  = try(var.settings.site_config.min_tls_version, null)
+      pre_warmed_instance_count        = try(var.settings.site_config.pre_warmed_instance_count, null)
+      runtime_scale_monitoring_enabled = try(var.settings.site_config.runtime_scale_monitoring_enabled, null)
+      use_32_bit_worker_process        = try(var.settings.site_config.use_32_bit_worker_process, null)
+      vnet_route_all_enabled           = try(var.settings.site_config.vnet_route_all_enabled, null)
+      websockets_enabled               = try(var.settings.site_config.websockets_enabled, null)
       dynamic "cors" {
-        for_each = try([site_config.value.cors], [])
+        for_each = can(var.settings.site_config.cors) ? [1] : []
         content {
-          allowed_origins     = try(cors.value.allowed_origins, [])
-          support_credentials = try(cors.value.support_credentials, null)
+          allowed_origins     = try(var.settings.site_config.cors.allowed_origins, [])
+          support_credentials = try(var.settings.site_config.cors.support_credentials, null)
         }
       }
     }
@@ -65,9 +65,9 @@ resource "azurerm_logic_app_standard" "main" {
   dynamic "connection_string" {
     for_each = can(var.settings.connection_string) ? [1] : []
     content {
-      name  = try(connection_string.value.name, null)
-      type  = try(connection_string.value.type, null)
-      value = try(connection_string.value.value, null)
+      name  = try(var.settings.connection_string.name, null)
+      type  = try(var.settings.connection_string.type, null)
+      value = try(var.settings.connection_string.value, null)
     }
   }
 }
