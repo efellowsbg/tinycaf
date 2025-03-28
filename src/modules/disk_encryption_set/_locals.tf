5cc5db951fab3ac9bd75224441d366a189534857
Lyudmil Ilchev <lilchev@efellows.bg>
2025-03-28 16:21:40 +0200

    fix recovery vault

--- src/modules/disk_encryption_set/_locals.tf
+++ src/modules/disk_encryption_set/_locals.tf
@@ -3,7 +3,7 @@ locals {
   resource_group_name = local.resource_group.name
   location            = local.resource_group.location
 
-  key_vault_key_id = var.resources.key_vault_keys[var.settings.key_vault_key_ref].resource_versionless_id
+  key_vault_key_id = var.resources.key_vault_keys[var.settings.key_vault_key_ref].versionless_id
   tags = merge(
     var.global_settings.tags,
     var.global_settings.inherit_resource_group_tags ? local.resource_group.tags : {},
