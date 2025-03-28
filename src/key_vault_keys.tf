a91aea2129e636a43312a52e006ab8c87d2f52d4
Lyudmil Ilchev <lilchev@efellows.bg>
2025-03-28 13:31:49 +0200

    remove role assignment on depends on

--- src/key_vault_keys.tf
+++ src/key_vault_keys.tf
@@ -9,6 +9,5 @@ module "key_vault_keys" {
     resource_groups  = module.resource_groups
     keyvaults        = module.keyvaults
     virtual_networks = module.virtual_networks
-    role_assignments = module.role_assignments
   }
 }
