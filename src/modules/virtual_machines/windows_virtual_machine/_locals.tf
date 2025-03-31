39b1b6bf28651a7a4e9714aa9f88a0c9f0e7e035
Lyudmil Ilchev <lilchev@efellows.bg>
2025-03-31 16:58:22 +0300

    fix pre commit

--- src/modules/virtual_machines/windows_virtual_machine/_locals.tf
+++ src/modules/virtual_machines/windows_virtual_machine/_locals.tf
@@ -4,7 +4,7 @@ locals {
   location            = local.resource_group.location
 
   network_interface_ids = module.network_interface.ids
-  key_vault_id = var.resources.keyvaults[var.settings.keyvault_ref].id
+  key_vault_id          = var.resources.keyvaults[var.settings.keyvault_ref].id
   tags = merge(
     var.global_settings.tags,
     var.global_settings.inherit_resource_group_tags ? local.resource_group.tags : {},
