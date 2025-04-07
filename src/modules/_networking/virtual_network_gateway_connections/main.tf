fc47e16288f9782b6626aaf5f9708d22fb5d5254
Lyudmil Ilchev <lilchev@efellows.bg>
2025-04-07 16:44:09 +0300

    add dpd timeout

--- src/modules/_networking/virtual_network_gateway_connections/main.tf
+++ src/modules/_networking/virtual_network_gateway_connections/main.tf
@@ -2,6 +2,7 @@ resource "azurerm_virtual_network_gateway_connection" "main" {
   name                = var.settings.name
   location            = local.location
   resource_group_name = local.resource_group_name
+  dpd_timeout_seconds = try(var.settings.dpd_timeout_seconds, null)
 
   virtual_network_gateway_id         = local.virtual_network_gateway_id
   local_network_gateway_id           = local.local_network_gateway_id
