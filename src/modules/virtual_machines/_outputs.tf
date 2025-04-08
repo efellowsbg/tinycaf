43d09d99e0836af697af0fce69f0ea436977a05f
Lyudmil Ilchev <lilchev@efellows.bg>
2025-04-08 11:38:49 +0300

    add minor change

--- src/modules/virtual_machines/_outputs.tf
+++ src/modules/virtual_machines/_outputs.tf
@@ -1,7 +1,13 @@
 output "windows_virtual_machines" {
-  value = length(module.windows_virtual_machines) > 0 ? [module.windows_virtual_machines[0].id] : []
+  value = length(module.windows_virtual_machines) > 0 ? {
+    for idx in range(length(module.windows_virtual_machines)) :
+    "windows_vm_${idx}" => module.windows_virtual_machines[idx]
+  } : {}
 }
 
 output "linux_virtual_machines" {
-  value = length(module.linux_virtual_machines) > 0 ? [module.linux_virtual_machines[0].id] : []
+  value = length(module.linux_virtual_machines) > 0 ? {
+    for idx in range(length(module.linux_virtual_machines)) :
+    "linux_vm_${idx}" => module.linux_virtual_machines[idx]
+  } : {}
 }
--- src/role_assignments.tf
+++ src/role_assignments.tf
@@ -4,15 +4,24 @@ module "role_assignments" {
   global_settings = local.global_settings
 
   resources = {
-    resource_groups        = module.resource_groups
-    keyvaults              = module.keyvaults
-    managed_identities     = module.managed_identities
-    kubernetes_clusters    = module.kubernetes_clusters
-    virtual_networks       = module.virtual_networks
-    storage_accounts       = module.storage_accounts
-    role_definitions       = module.role_definitions
-    disk_encryption_sets   = module.disk_encryption_sets
-    linux_virtual_machines = flatten([for vm in module.virtual_machines : vm.linux_virtual_machines])
-    windows_virtual_machines = flatten([for vm in module.virtual_machines : vm.windows_virtual_machines])
+    resource_groups      = module.resource_groups
+    keyvaults            = module.keyvaults
+    managed_identities   = module.managed_identities
+    kubernetes_clusters  = module.kubernetes_clusters
+    virtual_networks     = module.virtual_networks
+    storage_accounts     = module.storage_accounts
+    role_definitions     = module.role_definitions
+    disk_encryption_sets = module.disk_encryption_sets
+    linux_virtual_machines = {
+      for key, vm in module.virtual_machines :
+      key => vm.linux_virtual_machines[0]
+      if length(vm.linux_virtual_machines) > 0
+    }
+
+    windows_virtual_machines = {
+      for key, vm in module.virtual_machines :
+      key => vm.windows_virtual_machines[0]
+      if length(vm.windows_virtual_machines) > 0
+    }
   }
 }
