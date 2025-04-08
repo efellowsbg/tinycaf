171f69c3222acbf896a03c81ac0bdbbc8f28017a
Lyudmil Ilchev <lilchev@efellows.bg>
2025-04-08 11:21:08 +0300

    fix outputs

--- src/modules/virtual_machines/_outputs.tf
+++ src/modules/virtual_machines/_outputs.tf
@@ -1,7 +1,7 @@
-output "linux_virtual_machines" {
-  value = length(module.linux_virtual_machines) > 0 ? module.linux_virtual_machines[0].azurerm_linux_virtual_machine : []
+output "windows_virtual_machines" {
+  value = length(module.windows_virtual_machines) > 0 ? [module.windows_virtual_machines[0].id] : []
 }
 
-output "windows_virtual_machines" {
-  value = length(module.windows_virtual_machines) > 0 ? module.windows_virtual_machines[0].azurerm_windows_virtual_machine : []
+output "linux_virtual_machines" {
+  value = length(module.linux_virtual_machines) > 0 ? [module.linux_virtual_machines[0].id] : []
 }
