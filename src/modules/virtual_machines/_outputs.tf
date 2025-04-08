a29206f81f2d0f0b79114e006613faa86428ca42
Lyudmil Ilchev <lilchev@efellows.bg>
2025-04-08 11:16:12 +0300

    ad fix for virtual machines

--- src/modules/virtual_machines/_outputs.tf
+++ src/modules/virtual_machines/_outputs.tf
@@ -1,7 +1,7 @@
 output "linux_virtual_machines" {
-  value = module.linux_virtual_machines.azurerm_linux_virtual_machine
+  value = length(module.linux_virtual_machines) > 0 ? module.linux_virtual_machines[0].azurerm_linux_virtual_machine : []
 }
 
 output "windows_virtual_machines" {
-  value = module.windows_virtual_machines.azurerm_windows_virtual_machine
+  value = length(module.windows_virtual_machines) > 0 ? module.windows_virtual_machines[0].azurerm_windows_virtual_machine : []
 }
