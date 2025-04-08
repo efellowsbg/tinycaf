d815bc189609964061dfe26973d974869d0fe81e
Lyudmil Ilchev <lilchev@efellows.bg>
2025-04-08 10:00:23 +0300

    add windows virtual machine extension

---
+++ src/modules/virtual_machines/windows_virtual_machine/extensions.tf
@@ -0,0 +1,12 @@
+resource "azurerm_virtual_machine_extension" "extension" {
+  for_each = try(var.settings.extensions, {})
+
+  name                 = each.value.name
+  virtual_machine_id   = azurerm_windows_virtual_machine.main.id
+  publisher            = each.value.publisher
+  type                 = each.value.type
+  type_handler_version = each.value.type_handler_version
+
+  settings = try(each.value.settings,null)
+  tags = local.tags
+}
