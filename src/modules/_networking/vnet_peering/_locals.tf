730160fc5d28fcd8efce7d5e6b4cb6b73118237b
Lyudmil Ilchev <lilchev@efellows.bg>
2025-04-15 10:12:36 +0300

    fix vnet peering

--- src/modules/_networking/vnet_peering/_locals.tf
+++ src/modules/_networking/vnet_peering/_locals.tf
@@ -2,10 +2,11 @@ locals {
   vnet_left  = try(var.resources.virtual_networks[var.settings.vnet_left_ref], null)
   vnet_right = try(var.resources.virtual_networks[var.settings.vnet_right_ref], null)
 
-  direction          = try(var.settings.direction, "<->")
-  target             = local.direction == "target"
-  source             = local.direction == "source"
-  peer_left_to_right = endswith(local.direction, "->")
-  peer_right_to_left = startswith(local.direction, "<-")
+  direction = try(var.settings.direction, "<->")
+  target    = local.direction == "target"
+  source    = local.direction == "source"
 
+  # These use regex to simulate startswith/endswith
+  peer_left_to_right = can(regex("->$", local.direction))
+  peer_right_to_left = can(regex("^<-", local.direction))
 }
