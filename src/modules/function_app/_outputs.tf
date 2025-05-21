1ed639f72b264d29fe264ef7fff75ba3dcba86a2
Lyudmil Ilchev <lilchev@efellows.bg>
2025-05-21 14:57:47 +0300

    remove windows fa

--- src/modules/function_app/_outputs.tf
+++ src/modules/function_app/_outputs.tf
@@ -6,10 +6,10 @@ output "linux_function_apps" {
   } : {}
 }
 
-output "windows_function_apps" {
-  value = length(module.windows_function_apps) > 0 ? {
-    for key, fa in module.windows_function_apps :
-    key => fa
-    if length(fa) > 0
-  } : {}
-}
+# output "windows_function_apps" {
+#   value = length(module.windows_function_apps) > 0 ? {
+#     for key, fa in module.windows_function_apps :
+#     key => fa
+#     if length(fa) > 0
+#   } : {}
+# }
