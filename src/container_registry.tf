e20a341423e13a8969c130f2b7da0572ac2863f6
Lyudmil Ilchev <lilchev@efellows.bg>
2025-05-21 11:25:56 +0300

    add managed identities to resources

--- src/container_registry.tf
+++ src/container_registry.tf
@@ -10,5 +10,6 @@ module "container_registries" {
     virtual_networks  = module.virtual_networks
     private_dns_zones = module.private_dns_zones
     key_vault_keys = module.key_vault_keys
+    managed_identities = module.managed_identities
   }
 }
