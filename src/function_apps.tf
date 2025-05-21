8b05e808097048b28155bf2bdd6151ba7ffdb507
Lyudmil Ilchev <124342933+lyudmililchev92@users.noreply.github.com>
2025-05-21 15:03:48 +0300

    [TC-58] Add module for Linux Funciton App (#64)
    * TC-58 Add function app module support
    
    * rename folder
    
    * remove windows function apps
    
    * remove windows fa
    
    * fix pre commit

---
+++ src/function_apps.tf
@@ -0,0 +1,15 @@
+module "function_apps" {
+  source   = "./modules/function_app"
+  for_each = var.function_apps
+
+  settings        = each.value
+  global_settings = var.global_settings
+
+  resources = {
+    resource_groups   = module.resource_groups
+    virtual_networks  = module.virtual_networks
+    keyvaults         = module.keyvaults
+    app_service_plans = module.app_service_plans
+    storage_accounts  = module.storage_accounts
+  }
+}
