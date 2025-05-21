75b422cbe5098c8d0f50e0e1fdaee1445e02e2b1
Lyudmil Ilchev <lilchev@efellows.bg>
2025-05-21 13:46:43 +0300

    fix key vault key name

--- src/modules/storage_account/customer_managed_key.tf
+++ src/modules/storage_account/customer_managed_key.tf
@@ -4,5 +4,5 @@ resource "azurerm_storage_account_customer_managed_key" "cmk" {
 
   storage_account_id = azurerm_storage_account.main.id
   key_vault_id       = try(var.resources.key_vault_keys[var.settings.cmk_key].key_vault_id, null)
-  key_name           = try(var.resources.key_vault_keys[var.settings.cmk_key].key_name, null)
+  key_name           = try(var.resources.key_vault_keys[var.settings.cmk_key].name, null)
 }
