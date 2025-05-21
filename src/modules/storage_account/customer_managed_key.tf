220c1b23a869bed3d0b9ab81bcd5c1e803e9b7b3
Lyudmil Ilchev <lilchev@efellows.bg>
2025-05-21 13:39:27 +0300

    usage of cmk key

--- src/modules/storage_account/customer_managed_key.tf
+++ src/modules/storage_account/customer_managed_key.tf
@@ -1,8 +1,8 @@
 resource "azurerm_storage_account_customer_managed_key" "cmk" {
 
-  count = try(var.settings.customer_managed_key != null, false) ? 1 : 0
+  count = try(var.settings.cmk_key != null, false) ? 1 : 0
 
   storage_account_id = azurerm_storage_account.main.id
-  key_vault_id       = try(var.resources.key_vault_keys[var.settings.customer_managed_key.key_vault_key_ref].key_vault_id, null)
-  key_name           = try(var.resources.key_vault_keys[var.settings.customer_managed_key.key_vault_key_ref].key_name, null)
+  key_vault_id       = try(var.resources.key_vault_keys[var.settings.cmk_key].key_vault_id, null)
+  key_name           = try(var.resources.key_vault_keys[var.settings.cmk_key].key_name, null)
 }
