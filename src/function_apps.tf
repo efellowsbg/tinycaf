2f5c9d84d9e76c47904631078244a849e14a1844
Lyudmil Ilchev <lilchev@efellows.bg>
2025-05-21 15:02:52 +0300

    fix pre commit

--- src/function_apps.tf
+++ src/function_apps.tf
@@ -6,10 +6,10 @@ module "function_apps" {
   global_settings = var.global_settings
 
   resources = {
-    resource_groups      = module.resource_groups
-    virtual_networks     = module.virtual_networks
-    keyvaults            = module.keyvaults
+    resource_groups   = module.resource_groups
+    virtual_networks  = module.virtual_networks
+    keyvaults         = module.keyvaults
     app_service_plans = module.app_service_plans
-    storage_accounts     = module.storage_accounts
+    storage_accounts  = module.storage_accounts
   }
 }
--- src/modules/app_service_plan/main.tf
+++ src/modules/app_service_plan/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_service_plan" "main" {
-  name                = var.settings.name
-  resource_group_name = local.resource_group_name
-  location            = local.location
-  os_type             = var.settings.os_type
-  sku_name            = var.settings.sku_name
-  app_service_environment_id = try(var.settings.app_service_environment_id, null)
+  name                         = var.settings.name
+  resource_group_name          = local.resource_group_name
+  location                     = local.location
+  os_type                      = var.settings.os_type
+  sku_name                     = var.settings.sku_name
+  app_service_environment_id   = try(var.settings.app_service_environment_id, null)
   maximum_elastic_worker_count = try(var.settings.maximum_elastic_worker_count, null)
-  per_site_scaling_enabled = try(var.settings.per_site_scaling_enabled, null)
-  zone_balancing_enabled = try(var.settings.zone_balancing_enabled, null)
-  tags = local.tags
+  per_site_scaling_enabled     = try(var.settings.per_site_scaling_enabled, null)
+  zone_balancing_enabled       = try(var.settings.zone_balancing_enabled, null)
+  tags                         = local.tags
 }
