b5fac8b09612b80542d0d004ec1dfe699e4f781e
Lyudmil Ilchev <124342933+lyudmililchev92@users.noreply.github.com>
2025-05-21 14:02:46 +0300

    TC-51 Add support for customer managed key on storage account (#62)
    * [TC-51] Add support for cmk on storage account
    
    * add missing outputs
    
    * add custome rmanaged keys
    
    * usage of cmk key
    
    * fix key vault key name

--- src/modules/keyvault/key_vault_key/_outputs.tf
+++ src/modules/keyvault/key_vault_key/_outputs.tf
@@ -9,3 +9,11 @@ output "resource_versionless_id" {
 output "versionless_id" {
   value = azurerm_key_vault_key.main.versionless_id
 }
+
+output "key_vault_id" {
+  value = azurerm_key_vault_key.main.key_vault_id
+}
+
+output "name" {
+  value = azurerm_key_vault_key.main.name
+}
