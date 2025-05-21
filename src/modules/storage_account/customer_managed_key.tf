6f25cb572b1a8e3854f77b368d6ccf7c36abad50
Lyudmil Ilchev <lilchev@efellows.bg>
2025-05-21 13:36:48 +0300

    add custome rmanaged keys

--- src/modules/storage_account/customer_managed_key.tf
+++ src/modules/storage_account/customer_managed_key.tf
@@ -1,9 +1,6 @@
 resource "azurerm_storage_account_customer_managed_key" "cmk" {
 
-  count = (
-    try(var.settings.customer_managed_key != null, false) &&
-    try(var.settings.identity != null, false)
-  ) ? 1 : 0
+  count = try(var.settings.customer_managed_key != null, false) ? 1 : 0
 
   storage_account_id = azurerm_storage_account.main.id
   key_vault_id       = try(var.resources.key_vault_keys[var.settings.customer_managed_key.key_vault_key_ref].key_vault_id, null)
