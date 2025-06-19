931204f975f548c73b31e61f8dff5addfe2fc692
Kiril Kirilov <kiril.kirilov@efellows.bg>
2025-06-19 15:30:47 +0300

    fixed locals.tf

--- src/modules/_networking/application_gateway/_locals.tf
+++ src/modules/_networking/application_gateway/_locals.tf
@@ -6,9 +6,13 @@ locals {
   resource_group_name = local.resource_group.name
   location            = local.resource_group.location
 
+  first_frontend_ip_key = keys(var.settings.frontend_ip_configuration)[0]
+
   public_ip = var.resources[
-    try(var.settings.public_ip_lz_key, var.client_config.landingzone_key)
-  ].public_ips[var.settings.public_ip]
+    try(var.settings.frontend_ip_configuration[local.first_frontend_ip_key].lz_key, var.client_config.landingzone_key)
+  ].public_ips[
+    var.settings.frontend_ip_configuration[local.first_frontend_ip_key].public_ip
+  ]
 
   subnet = var.resources[
     try(var.settings.subnet_lz_key, var.client_config.landingzone_key)
