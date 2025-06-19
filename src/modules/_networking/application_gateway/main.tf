74f01e6d8576bbae230720804ecd2bf68cd7603d
Kiril Kirilov <kiril.kirilov@efellows.bg>
2025-06-19 15:07:16 +0300

    updated main.tf

--- src/modules/_networking/application_gateway/main.tf
+++ src/modules/_networking/application_gateway/main.tf
@@ -11,27 +11,31 @@ resource "azurerm_application_gateway" "main" {
   }
 
   dynamic "gateway_ip_configuration" {
-  for_each = var.settings.gateway_ip_configuration
-  content {
-    name      = gateway_ip_configuration.value.name
-    subnet_id = var.resources[
-      try(var.settings.lz_key, var.client_config.landingzone_key)
-    ].virtual_networks[
-      split("/", gateway_ip_configuration.value.subnet_ref)[0]
-    ].subnets[
-      split("/", gateway_ip_configuration.value.subnet_ref)[1]
-    ].id
+    for_each = try(var.settings.gateway_ip_configuration, {})
+    content {
+      name      = gateway_ip_configuration.value.name
+      subnet_id = var.resources[
+        try(gateway_ip_configuration.value.lz_key, var.client_config.landingzone_key)
+      ].virtual_networks[
+        split("/", gateway_ip_configuration.value.subnet_ref)[0]
+      ].subnets[
+        split("/", gateway_ip_configuration.value.subnet_ref)[1]
+      ].id
+    }
   }
-}
 
-
-  frontend_ip_configuration {
-    name                 = var.settings.frontend_ip_configuration.name
-    public_ip_address_id = local.public_ip.id
+  dynamic "frontend_ip_configuration" {
+    for_each = try(var.settings.frontend_ip_configuration, {})
+    content {
+      name                 = frontend_ip_configuration.value.name
+      public_ip_address_id = var.resources[
+        try(frontend_ip_configuration.value.lz_key, var.client_config.landingzone_key)
+      ].public_ips[frontend_ip_configuration.value.public_ip].id
+    }
   }
 
   dynamic "frontend_port" {
-    for_each = try(var.settings.frontend_ports, [])
+    for_each = try(var.settings.frontend_ports, {})
     content {
       name = frontend_port.value.name
       port = frontend_port.value.port
@@ -39,7 +43,7 @@ resource "azurerm_application_gateway" "main" {
   }
 
   dynamic "backend_address_pool" {
-    for_each = try(var.settings.backend_address_pools, [])
+    for_each = try(var.settings.backend_address_pools, {})
     content {
       name         = backend_address_pool.value.name
       ip_addresses = try(backend_address_pool.value.ip_addresses, [])
@@ -47,7 +51,7 @@ resource "azurerm_application_gateway" "main" {
   }
 
   dynamic "backend_http_settings" {
-    for_each = try(var.settings.backend_http_settings_list, [])
+    for_each = try(var.settings.backend_http_settings_list, {})
     content {
       name                  = backend_http_settings.value.name
       port                  = backend_http_settings.value.port
@@ -58,7 +62,7 @@ resource "azurerm_application_gateway" "main" {
   }
 
   dynamic "http_listener" {
-    for_each = try(var.settings.http_listeners, [])
+    for_each = try(var.settings.http_listeners, {})
     content {
       name                           = http_listener.value.name
       frontend_ip_configuration_name = http_listener.value.frontend_ip_configuration_name
@@ -69,13 +73,14 @@ resource "azurerm_application_gateway" "main" {
   }
 
   dynamic "request_routing_rule" {
-    for_each = try(var.settings.request_routing_rules, [])
+    for_each = try(var.settings.request_routing_rules, {})
     content {
       name                       = request_routing_rule.value.name
       rule_type                  = request_routing_rule.value.rule_type
       http_listener_name         = request_routing_rule.value.http_listener_name
       backend_address_pool_name  = request_routing_rule.value.backend_address_pool_name
       backend_http_settings_name = request_routing_rule.value.backend_http_settings_name
+      priority                   = try(request_routing_rule.value.priority, 100)
     }
   }
 
