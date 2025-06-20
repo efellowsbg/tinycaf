fbc9681c37c333e20e190a1fc339c1ffc411b1d5
Kiril Kirilov <kiril.kirilov@efellows.bg>
2025-06-20 11:43:37 +0300

    typo

--- src/_variables.resources.tf
+++ src/_variables.resources.tf
@@ -86,6 +86,4 @@ variable "logs_destination_ids" { default = {} }
 
 variable "log_analytics_destination_type" { default = {} }
 
-variable "resources" { default = {} }
-
 variable "settings" { default = {} }
