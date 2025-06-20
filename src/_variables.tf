e9b40fdcb7d30cc86b3ffb1b6fd101b10c665ad2
Kiril Kirilov <kiril.kirilov@efellows.bg>
2025-06-20 12:53:20 +0300

    Updated variables

--- src/_variables.tf
+++ src/_variables.tf
@@ -79,3 +79,32 @@ variable "storage_account_name" {
   default     = "projectstoragedemo"
 }
 
+variable "diagnostic_setting" {
+  description = "Diagnostic setting configuration passed to module"
+  type = object({
+    name                       = string
+    resource_type              = string
+    resource_ref               = string
+    resource_lz_key            = optional(string)
+    storage_account_ref        = optional(string)
+    storage_account_lz_key     = optional(string)
+    log_analytics_workspace_ref = optional(string)
+    log_analytics_lz_key       = optional(string)
+    logs = optional(map(object({
+      category         = string
+      enabled          = bool
+      retention_policy = optional(object({
+        enabled = bool
+        days    = number
+      }))
+    })))
+    metrics = optional(map(object({
+      category         = string
+      enabled          = bool
+      retention_policy = optional(object({
+        enabled = bool
+        days    = number
+      }))
+    })))
+  })
+}
