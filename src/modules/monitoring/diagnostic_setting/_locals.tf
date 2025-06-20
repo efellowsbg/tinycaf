c554c110ae4c72337169be40525d3a94260e9f35
Kiril Kirilov <kiril.kirilov@efellows.bg>
2025-06-20 14:08:24 +0300

    updated locals

--- src/modules/monitoring/diagnostic_setting/_locals.tf
+++ src/modules/monitoring/diagnostic_setting/_locals.tf
@@ -1,4 +1,4 @@
 locals {
-  use_storage = try(var.settings.storage_account_ref, null) != null
-  use_law     = try(var.settings.log_analytics_workspace_ref, null) != null
+  use_storage = try(var.diagnostic_setting.storage_account_ref, null) != null
+  use_law     = try(var.diagnostic_setting.log_analytics_workspace_ref, null) != null
 }
