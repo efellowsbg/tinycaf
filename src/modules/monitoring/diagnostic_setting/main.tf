7b91c7a6de4f72170056690004594c4eac5c5ad0
Kiril Kirilov <kiril.kirilov@efellows.bg>
2025-06-20 14:15:34 +0300

    updated main.tf

--- src/modules/monitoring/diagnostic_setting/main.tf
+++ src/modules/monitoring/diagnostic_setting/main.tf
@@ -1,6 +1,5 @@
 resource "azurerm_monitor_diagnostic_setting" "main" {
-  name = var.diagnostic_setting.name
-
+  name               = var.diagnostic_setting.name
   target_resource_id = var.resources[
     try(var.diagnostic_setting.resource_lz_key, var.client_config.landingzone_key)
   ][var.diagnostic_setting.resource_type][var.diagnostic_setting.resource_ref].id
@@ -11,6 +10,10 @@ resource "azurerm_monitor_diagnostic_setting" "main" {
       category = log.value.category
       enabled  = log.value.enabled
 
+      retention_policy {
+        enabled = try(log.value.retention_policy.enabled, false)
+        days    = try(log.value.retention_policy.days, 0)
+      }
     }
   }
 
@@ -23,21 +26,21 @@ resource "azurerm_monitor_diagnostic_setting" "main" {
     }
   }
 
-  dynamic "log_analytics_workspace_id" {
-    for_each = try(var.diagnostic_setting.log_analytics_workspace_ref, null) != null ? [1] : []
-    content {
-      workspace_id = var.resources[
-        try(var.diagnostic_setting.log_analytics_lz_key, var.client_config.landingzone_key)
-      ].log_analytics[var.diagnostic_setting.log_analytics_workspace_ref].id
-    }
-  }
+  # Only include if log_analytics_workspace_ref is not null
+  log_analytics_workspace_id = (
+    try(var.diagnostic_setting.log_analytics_workspace_ref, null) != null ?
+    var.resources[
+      try(var.diagnostic_setting.log_analytics_lz_key, var.client_config.landingzone_key)
+    ].log_analytics[var.diagnostic_setting.log_analytics_workspace_ref].id :
+    null
+  )
 
-  dynamic "storage_account_id" {
-    for_each = try(var.diagnostic_setting.storage_account_ref, null) != null ? [1] : []
-    content {
-      storage_account_id = var.resources[
-        try(var.diagnostic_setting.storage_account_lz_key, var.client_config.landingzone_key)
-      ].storage_accounts[var.diagnostic_setting.storage_account_ref].id
-    }
-  }
+  # Only include if storage_account_ref is not null
+  storage_account_id = (
+    try(var.diagnostic_setting.storage_account_ref, null) != null ?
+    var.resources[
+      try(var.diagnostic_setting.storage_account_lz_key, var.client_config.landingzone_key)
+    ].storage_accounts[var.diagnostic_setting.storage_account_ref].id :
+    null
+  )
 }
