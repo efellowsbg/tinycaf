cbf79249ea8deacd90d34bda021c39695ca40530
Lyudmil Ilchev <lilchev@efellows.bg>
2025-06-25 17:42:23 +0300

    fix naming

--- src/modules/keyvault/keyvault_private_endpoint/main.tf
+++ src/modules/keyvault/keyvault_private_endpoint/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_private_endpoint" "main" {
-  name                = "pe-${var.settings.name}"
+  name                = try(var.settings.private_endpoint.name,"pe-${var.settings.name}")
   resource_group_name = local.resource_group_name
   location            = local.location
   subnet_id           = local.subnet_id
@@ -7,7 +7,7 @@ resource "azurerm_private_endpoint" "main" {
   tags = local.tags
 
   private_service_connection {
-    name                           = "psc-${var.settings.name}"
+    name                           = try(var.settings.private_endpoint.private_service_connection.name,"psc-${var.settings.name}")
     private_connection_resource_id = var.keyvault_id
 
     is_manual_connection              = try(var.settings.private_endpoint.private_service_connection.is_manual_connection, false)
