80d6394ff94e8b0c3d84f986d8700881fd53f77b
Lyudmil Ilchev <lilchev@efellows.bg>
2025-06-27 14:42:10 +0300

    Add possibility to add All

--- src/modules/keyvault/keyvault_access_policy/_locals.tf
+++ src/modules/keyvault/keyvault_access_policy/_locals.tf
@@ -1,26 +1,45 @@
 locals {
+  all_key_permissions = [
+    "Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import",
+    "List", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey", "Release", "Rotate"
+  ]
+
+  all_secret_permissions = [
+    "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"
+  ]
+
+  all_certificate_permissions = [
+    "Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List",
+    "ListIssuers", "ManageContacts", "ManageIssuers", "Purge", "Recover", "Restore", "SetIssuers", "Update"
+  ]
+
+  all_storage_permissions = [
+    "Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey",
+    "Restore", "Set", "SetSAS", "Update"
+  ]
+
   normalized_access_policies = flatten([
     concat(
       [
         for ref in try(var.access_policies.managed_identity_refs, []) : {
-          key = "${var.policy_name}_managed_identity_${ref}"
-          object_id = var.resources[
-            try(var.settings.managed_identity_lz_key, var.client_config.landingzone_key)
-          ].managed_identities[ref].principal_id
-          key_permissions         = try(var.access_policies.key_permissions, [])
-          secret_permissions      = try(var.access_policies.secret_permissions, [])
-          certificate_permissions = try(var.access_policies.certificate_permissions, [])
-          storage_permissions     = try(var.access_policies.storage_permissions, [])
+          key                     = "${var.policy_name}_managed_identity_${ref}"
+          object_id               = var.resources[
+                                      try(var.settings.managed_identity_lz_key, var.client_config.landingzone_key)
+                                    ].managed_identities[ref].principal_id
+          key_permissions         = contains(try(var.access_policies.key_permissions, []), "All") ? local.all_key_permissions : try(var.access_policies.key_permissions, [])
+          secret_permissions      = contains(try(var.access_policies.secret_permissions, []), "All") ? local.all_secret_permissions : try(var.access_policies.secret_permissions, [])
+          certificate_permissions = contains(try(var.access_policies.certificate_permissions, []), "All") ? local.all_certificate_permissions : try(var.access_policies.certificate_permissions, [])
+          storage_permissions     = contains(try(var.access_policies.storage_permissions, []), "All") ? local.all_storage_permissions : try(var.access_policies.storage_permissions, [])
         }
       ],
       [
         for obj_id in try(var.access_policies.object_ids, []) : {
           key                     = "${var.policy_name}_object_id_${obj_id}"
           object_id               = obj_id
-          key_permissions         = try(var.access_policies.key_permissions, [])
-          secret_permissions      = try(var.access_policies.secret_permissions, [])
-          certificate_permissions = try(var.access_policies.certificate_permissions, [])
-          storage_permissions     = try(var.access_policies.storage_permissions, [])
+          key_permissions         = contains(try(var.access_policies.key_permissions, []), "All") ? local.all_key_permissions : try(var.access_policies.key_permissions, [])
+          secret_permissions      = contains(try(var.access_policies.secret_permissions, []), "All") ? local.all_secret_permissions : try(var.access_policies.secret_permissions, [])
+          certificate_permissions = contains(try(var.access_policies.certificate_permissions, []), "All") ? local.all_certificate_permissions : try(var.access_policies.certificate_permissions, [])
+          storage_permissions     = contains(try(var.access_policies.storage_permissions, []), "All") ? local.all_storage_permissions : try(var.access_policies.storage_permissions, [])
         }
       ]
     )
