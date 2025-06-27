36703ff96ecb4acd74b53cc0b9178170f6df37a8
Lyudmil Ilchev <lilchev@efellows.bg>
2025-06-27 14:38:48 +0300

    fix access policies

--- src/modules/keyvault/keyvault_access_policy/_locals.tf
+++ src/modules/keyvault/keyvault_access_policy/_locals.tf
@@ -1,49 +1,4 @@
 locals {
-  all_secret_permissions = [
-    "Backup",
-    "Delete",
-    "Get",
-    "List",
-    "Purge",
-    "Recover",
-    "Restore",
-    "Set",
-  ]
-
-  all_key_permissions = [
-    "Backup",
-    "Create",
-    "Decrypt",
-    "Delete",
-    "Encrypt",
-    "Get",
-    "Import",
-    "List",
-    "Purge",
-    "Recover",
-    "Restore",
-    "Sign",
-    "UnwrapKey",
-    "Update",
-    "Verify",
-    "WrapKey",
-    "Release",
-    "Rotate",
-    "GetRotationPolicy",
-    "SetRotationPolicy",
-  ]
-  all_certificate_permissions = [
-    "Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import",
-    "List", "ManageContacts", "ManageIssuers", "Purge", "Recover", "Restore",
-    "SetIssuers", "Update",
-  ]
-
-  debug_settings    = var.settings
-  has_logged_in_key = contains(keys(var.settings), "managed_identity")
-}
-
-
-locals {
   normalized_access_policies = flatten([
     concat(
       [
--- src/modules/keyvault/keyvault_access_policy/access_policy/main.tf
+++ src/modules/keyvault/keyvault_access_policy/access_policy/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_key_vault_access_policy" "main" { # Using the policy key in the resource name
+resource "azurerm_key_vault_access_policy" "main" {
   key_vault_id       = var.keyvault_id
   tenant_id          = var.tenant_id
   object_id          = var.object_id
