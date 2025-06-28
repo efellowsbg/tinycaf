225e8fd6569bbd5c7f61d13bbefcb73003ba9d49
Lyudmil Ilchev <lilchev@efellows.bg>
2025-06-28 16:07:48 +0300

    fix modules

--- src/modules/_networking/subnet_route_table_association/main.tf
+++ src/modules/_networking/subnet_route_table_association/main.tf
@@ -1,4 +1,4 @@
-resource "subnet_route_table_association" "main" {
+resource "azurerm_subnet_route_table_association" "main" {
   subnet_id      = local.subnet_id
   route_table_id = local.route_table_id
 }
