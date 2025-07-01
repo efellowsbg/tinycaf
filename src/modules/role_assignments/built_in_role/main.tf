f96c1cdb84a3476446c46590c8d2cd4d8f832242
Lyudmil Ilchev <lilchev@efellows.bg>
2025-07-01 16:14:06 +0300

    fix resource type of subnets

--- src/modules/role_assignments/built_in_role/main.tf
+++ src/modules/role_assignments/built_in_role/main.tf
@@ -23,6 +23,11 @@ resource "azurerm_role_assignment" "main" {
   })
 
   scope = try(
+    each.value.resource_type == "subnets" ?
+    var.resources[var.client_config.landingzone_key]
+    .virtual_networks[split("/", each.value.resource_key)[0]]
+    .subnets[split("/", each.value.resource_key)[1]]
+    .id :
     var.resources[each.value.resource_type][each.value.resource_key].id,
     null
   )
