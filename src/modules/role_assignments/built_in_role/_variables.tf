b0dd55fd0d1e0338dbe48b79fe34fdeafc20fb51
Lyudmil Ilchev <lilchev@efellows.bg>
2025-07-01 16:21:59 +0300

    add client config

--- src/modules/role_assignments/built_in_role/_variables.tf
+++ src/modules/role_assignments/built_in_role/_variables.tf
@@ -2,6 +2,10 @@ variable "global_settings" {
   description = "Global settings for tinycaf"
 }
 
+variable "client_config" {
+  description = "Global settings for tinycaf"
+}
+
 variable "settings" {
   description = "All the configuration for this resource"
 }
