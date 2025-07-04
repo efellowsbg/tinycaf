cda0e8d24e1da56319f3f9151b2ee48903af38fc
Lyudmil Ilchev <lilchev@efellows.bg>
2025-07-04 15:59:36 +0300

    fix tags

--- src/modules/managed_identity/main.tf
+++ src/modules/managed_identity/main.tf
@@ -2,4 +2,5 @@ resource "azurerm_user_assigned_identity" "main" {
   name                = var.settings.name
   resource_group_name = local.resource_group_name
   location            = local.location
+  tags = local.tags
 }
