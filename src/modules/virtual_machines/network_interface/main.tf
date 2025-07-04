640de27e591cfd5b1e7b400d882b57479c10ff56
Lyudmil Ilchev <lilchev@efellows.bg>
2025-07-04 13:18:48 +0300

    add nsg association

--- src/modules/virtual_machines/network_interface/main.tf
+++ src/modules/virtual_machines/network_interface/main.tf
@@ -34,3 +34,18 @@ resource "azurerm_network_interface" "main" {
     )
   }
 }
+
+
+resource "azurerm_network_interface_security_group_association" "main" {
+  for_each = {
+    for key, val in var.settings.network_interfaces :
+    key => val if try(val.network_security_group_ref, null) != null && try(val.network_security_group_ref, "") != ""
+  }
+
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = var.resources[
+    try(each.value.network_security_group_lz_key, var.client_config.landingzone_key)
+  ].network_security_groups[
+    each.value.network_security_group_ref
+  ].id
+}
