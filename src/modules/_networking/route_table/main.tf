6df22e7fe05716901d8474a589ee9bf19ec43b38
Lyudmil Ilchev <lilchev@efellows.bg>
2025-07-04 14:31:59 +0300

    add subnet route table here inside

--- src/modules/_networking/route_table/main.tf
+++ src/modules/_networking/route_table/main.tf
@@ -15,3 +15,20 @@ resource "azurerm_route_table" "main" {
     }
   }
 }
+
+
+resource "azurerm_subnet_route_table_association" "main" {
+  for_each = var.settings.subnet_refs != null && length(var.settings.subnet_refs) > 0 ? {
+    for idx, subnet_id in var.settings.subnet_refs : idx => subnet_id
+  } : {}
+
+  subnet_id      = var.resources[var.client_config.landingzone_key
+  ].virtual_networks[
+    split("/", each.value)[0]
+  ].subnets[
+    split("/", each.value)[1]
+  ].id
+  route_table_id = azurerm_route_table.main.id
+}
+
+
