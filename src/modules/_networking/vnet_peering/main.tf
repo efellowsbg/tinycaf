3ca99096dd81d0794e360bcf862364d34ee9352d
Lyudmil Ilchev <lilchev@efellows.bg>
2025-07-04 13:57:25 +0300

    fix

--- src/modules/_networking/vnet_peering/main.tf
+++ src/modules/_networking/vnet_peering/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network_peering" "left_to_right" {
   count = local.peer_left_to_right ? 1 : 0
 
-  name                      = "peering-${local.vnet_left.name}-to-${local.vnet_right.name}"
+  name                      = try(var.settings.name_left_to_right,"peering-${local.vnet_left.name}-to-${local.vnet_right.name}")
   resource_group_name       = local.vnet_left.resource_group_name
   virtual_network_name      = local.vnet_left.name
   remote_virtual_network_id = local.vnet_right.id
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network_peering" "left_to_right" {
 resource "azurerm_virtual_network_peering" "right_to_left" {
   count = local.peer_right_to_left ? 1 : 0
 
-  name                      = "peering-${local.vnet_right.name}-to-${local.vnet_left.name}"
+  name                      = try(var.settings.name_right_to_left,"peering-${local.vnet_right.name}-to-${local.vnet_left.name}")
   resource_group_name       = local.vnet_right.resource_group_name
   virtual_network_name      = local.vnet_right.name
   remote_virtual_network_id = local.vnet_left.id
@@ -19,7 +19,7 @@ resource "azurerm_virtual_network_peering" "right_to_left" {
 resource "azurerm_virtual_network_peering" "target" {
   count = local.target ? 1 : 0
 
-  name                         = "peering-${local.vnet_right.name}"
+  name                         = try(var.settings.name_target,"peering-${local.vnet_right.name}")
   resource_group_name          = local.vnet_right.resource_group_name
   virtual_network_name         = local.vnet_right.name
   remote_virtual_network_id    = try(var.settings.remote_vnet_id, local.vnet_left.id)
