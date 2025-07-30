2f525586f38e02dba5474a6ce9de64fc960bb373
Lyudmil Ilchev <lilchev@efellows.bg>
2025-07-30 15:51:23 +0300

    fix azapi

--- src/_provider.tf
+++ src/_provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.33.0"
     }
+    azapi = {
+      source = "Azure/azapi"
+      version = "2.5.0" // version
+    }
   }
 }
 
@@ -14,15 +18,6 @@ provider "azurerm" {
   client_id       = var.client_id
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
-}
-
-terraform {
-  required_providers {
-    azapi = {
-      source = "Azure/azapi"
-      version = "2.5.0" // version
-    }
-  }
 }
 
 provider "azapi" {
