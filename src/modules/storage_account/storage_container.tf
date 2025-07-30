bc2ebd33a96650557dc9547803bf5f648d7bc60d
Lyudmil Ilchev <lilchev@efellows.bg>
2025-07-30 15:18:17 +0300

    add az api resource

--- src/modules/storage_account/storage_container.tf
+++ src/modules/storage_account/storage_container.tf
@@ -6,3 +6,22 @@ resource "azurerm_storage_container" "main" {
 
   container_access_type = try(each.value.access_type, null)
 }
+
+
+resource "azapi_resource" "main" {
+  for_each = try(var.settings.api_containers, {})
+
+  type      = "Microsoft.Storage/storageAccounts/blobServices/containers@2023-05-01"
+  name      = each.value.name
+  parent_id = "${azurerm_storage_account.main.id}/blobServices/default"
+  body = {
+    properties = {
+      defaultEncryptionScope      = try(each.value.default_encryption_scope,"$account-encryption-key")
+      denyEncryptionScopeOverride = try(each.value.deny_encryption_scope_override,false)
+      immutableStorageWithVersioning = {
+        enabled = try(each.value.enable_versioning,true)
+      }
+      publicAccess = try(each.value.public_access,"None")
+    }
+  }
+}
