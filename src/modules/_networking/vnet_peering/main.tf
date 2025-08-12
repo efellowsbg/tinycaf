a8569628a2ed05c438a7256aa9da52a1fd509771
Borislav Raynov <braynov@efellows.bg>
2025-08-12 10:51:45 +0300

    Add missing optional param

--- src/modules/_networking/vnet_peering/main.tf
+++ src/modules/_networking/vnet_peering/main.tf
@@ -5,6 +5,7 @@ resource "azurerm_virtual_network_peering" "left_to_right" {
   resource_group_name       = local.vnet_left.resource_group_name
   virtual_network_name      = local.vnet_left.name
   remote_virtual_network_id = local.vnet_right.id
+  use_remote_gateways       = try(var.settings.use_remote_gateways, true)
 }
 
 resource "azurerm_virtual_network_peering" "right_to_left" {
@@ -14,6 +15,7 @@ resource "azurerm_virtual_network_peering" "right_to_left" {
   resource_group_name       = local.vnet_right.resource_group_name
   virtual_network_name      = local.vnet_right.name
   remote_virtual_network_id = local.vnet_left.id
+  allow_gateway_transit     = try(var.settings.allow_gateway_transit, true)
 }
 
 resource "azurerm_virtual_network_peering" "target" {
