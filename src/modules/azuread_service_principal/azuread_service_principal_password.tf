f916ed9b0e0a47552629f808bd8b441dfa44148b
Lyudmil Ilchev <lilchev@efellows.bg>
2025-08-14 17:32:41 +0300

    fix sp

--- src/modules/azuread_service_principal/azuread_service_principal_password.tf
+++ src/modules/azuread_service_principal/azuread_service_principal_password.tf
@@ -1,18 +1,18 @@
-
-
 resource "azuread_service_principal_password" "main" {
   count                = var.create_password ? 1 : 0
   service_principal_id = azuread_service_principal.main.id
 }
 
-resource "azurerm_key_vault_secret" "public_keys" {
-  count                = var.create_password ? 1 : 0
-  name         = "${azuread_service_principal_password.main.display_name}"
-  value        = azuread_service_principal_password.main.value
-  key_vault_id = try(var.resources[
-    try(var.settings.keyvault_lz_key, var.client_config.landingzone_key)
+resource "azurerm_key_vault_secret" "main" {
+  count        = var.create_password ? 1 : 0
+  name         = azuread_service_principal_password.main[count.index].display_name
+  value        = azuread_service_principal_password.main[count.index].value
+  key_vault_id = try(
+    var.resources[
+      try(var.settings.keyvault_lz_key, var.client_config.landingzone_key)
     ].keyvaults[
-    var.settings.keyvault_ref
-  ].id, null)
+      var.settings.keyvault_ref
+    ].id,
+    null
+  )
 }
-
