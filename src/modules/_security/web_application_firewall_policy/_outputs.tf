beb0a4ad71ee7d9b5795dc7130bc02186792ec9b
Borislav Raynov <braynov@efellows.bg>
2025-08-14 11:45:27 +0300

    Add output in module

--- src/modules/_security/web_application_firewall_policy/_outputs.tf
+++ src/modules/_security/web_application_firewall_policy/_outputs.tf
@@ -1,3 +1,13 @@
 output "id" {
   value = azurerm_web_application_firewall_policy.main.id
 }
+
+output "waf_policies" {
+  value = {
+    for k, v in azurerm_web_application_firewall_policy.main :
+    k => {
+      id   = v.id
+      name = v.name
+    }
+  }
+}
