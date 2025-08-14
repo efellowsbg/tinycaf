f0a32bbeccc620f735811b945a5a77277f63cc98
Lyudmil Ilchev <lilchev@efellows.bg>
2025-08-14 17:28:25 +0300

    add pass

--- src/modules/azuread_service_principal/azuread_service_principal_password.tf
+++ src/modules/azuread_service_principal/azuread_service_principal_password.tf
@@ -1,11 +1,18 @@
-# resource "time_rotating" "main" {
-#   rotation_days = 7
-# }
+
 
 resource "azuread_service_principal_password" "main" {
   count                = var.create_password ? 1 : 0
   service_principal_id = azuread_service_principal.main.id
-  #  rotate_when_changed = {
-  #   rotation = time_rotating.main.id
-  # }
 }
+
+resource "azurerm_key_vault_secret" "public_keys" {
+  count                = var.create_password ? 1 : 0
+  name         = "${azuread_service_principal_password.main.display_name}"
+  value        = azuread_service_principal_password.main.value
+  key_vault_id = try(var.resources[
+    try(var.settings.keyvault_lz_key, var.client_config.landingzone_key)
+    ].keyvaults[
+    var.settings.keyvault_ref
+  ].id, null)
+}
+
