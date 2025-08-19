56152df0b6c248eed95d74fc7dedb403c431d84c
Lyudmil Ilchev <lilchev@efellows.bg>
2025-08-19 13:12:28 +0300

    add managed disks

--- src/modules/virtual_machines/linux_virtual_machine/managed_disks.tf
+++ src/modules/virtual_machines/linux_virtual_machine/managed_disks.tf
@@ -1,9 +1,24 @@
-module "managed_disks" {
-  source = "../../managed_disks"
-  for_each = var.settings.data_disks
+resource "azurerm_managed_disk" "main" {
+  for_each             = try(var.settings.data_disks, {})
 
-  settings        = each.value
-  global_settings = var.global_settings
-  resources       = var.resources
-  client_config   = var.client_config
+  name                 = each.value.name
+  location             = local.location
+  resource_group_name  = local.resource_group_name
+  storage_account_type = try(each.value.storage_account_type, "Standard_LRS")
+  create_option        = try(each.value.create_option, "Empty")
+  disk_size_gb         = try(each.value.disk_size_gb, 10)
+  zone                 = try(each.value.zone, null)
+  tier                 = try(each.value.tier, null)
+  tags                 = local.tags
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "main" {
+  for_each = try(var.settings.data_disks, {})
+
+  managed_disk_id   = azurerm_managed_disk.main[each.key].id
+  virtual_machine_id = azurerm_linux_virtual_machine.main.id
+  lun                = each.value.lun
+  write_accelerator_enabled = try(each.value.write_accelerator_enabled, null)
+  caching            = try(each.value.caching, "None")
+  create_option      = try(each.value.create_option, null)
 }
