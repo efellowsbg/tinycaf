f138e6f3b5f6d64b5cc017d9ff85c9c5d254e18d
Lyudmil Ilchev <lilchev@efellows.bg>
2025-09-10 11:14:07 +0300

    revert changes

--- src/modules/diagnostic_setting/_locals.tf
+++ src/modules/diagnostic_setting/_locals.tf
@@ -1,11 +1,5 @@
 locals {
 
-  tags = merge(
-    var.global_settings.tags,
-    var.global_settings.inherit_resource_group_tags ? local.resource_group.tags : {},
-    try(var.settings.tags, {})
-  )
-
   log_analytics_workspace_id = coalesce(
     try(var.resources[
       try(var.settings.log_analytics_lz_key, var.client_config.landingzone_key)
