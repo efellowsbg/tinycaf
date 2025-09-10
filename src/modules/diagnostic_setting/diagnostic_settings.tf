1acee541d29160c419c2ed797879e46957e83ace
Lyudmil Ilchev <124342933+lyudmililchev92@users.noreply.github.com>
2025-09-10 16:02:34 +0300

    [TC-66] Add support for azurerm_monitor_diagnostic_setting (#83)
    TC-66 add diagnostic setting

---
+++ src/modules/diagnostic_setting/diagnostic_settings.tf
@@ -0,0 +1,20 @@
+resource "azurerm_monitor_diagnostic_setting" "main" {
+  name = var.settings.name
+  target_resource_id = var.resources[
+    try(var.settings.target_lz_key, var.client_config.landingzone_key)
+  ][var.settings.target_resource_type][var.settings.target_resource_ref].id
+  log_analytics_workspace_id = try(local.log_analytics_workspace_id, null)
+  storage_account_id         = try(local.storage_account_id, null)
+  dynamic "enabled_log" {
+    for_each = try(var.settings.logs, {})
+    content {
+      category = enabled_log.value.category
+    }
+  }
+  dynamic "enabled_metric" {
+    for_each = try(var.settings.metrics, {})
+    content {
+      category = enabled_metric.value.category
+    }
+  }
+}
