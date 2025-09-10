a066d240f9faa2d5a52fd3608faf0223870739ce
Lyudmil Ilchev <lilchev@efellows.bg>
2025-09-10 15:08:58 +0300

    fix resource type

--- src/modules/diagnostic_setting/diagnostic_settings.tf
+++ src/modules/diagnostic_setting/diagnostic_settings.tf
@@ -2,7 +2,7 @@ resource "azurerm_monitor_diagnostic_setting" "main" {
   name = var.settings.name
   target_resource_id = var.resources[
     try(var.settings.target_lz_key, var.client_config.landingzone_key)
-  ].var.settings.target_resource_type[var.settings.target_resource_ref].id
+  ][var.settings.target_resource_type][var.settings.target_resource_ref].id
   log_analytics_workspace_id = local.log_analytics_workspace_id
   dynamic "enabled_log" {
     for_each = try(var.settings.logs, {})
