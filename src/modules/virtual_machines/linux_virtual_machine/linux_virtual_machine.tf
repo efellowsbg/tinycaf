b6255eb9b1b8f59fe97378932b5a41468c83a955
Lyudmil Ilchev <lilchev@efellows.bg>
2025-10-22 15:47:37 +0300

    fix blocks

--- src/modules/virtual_machines/linux_virtual_machine/linux_virtual_machine.tf
+++ src/modules/virtual_machines/linux_virtual_machine/linux_virtual_machine.tf
@@ -2,7 +2,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   name                            = var.settings.name
   resource_group_name             = local.resource_group_name
   location                        = local.location
-  admin_username                  = try(var.settings.admin_username,null)
+  admin_username                  = try(var.settings.admin_username, null)
   admin_password                  = try(random_password.admin[0].result, null)
   size                            = var.settings.size
   network_interface_ids           = local.network_interface_ids
@@ -28,15 +28,14 @@ resource "azurerm_linux_virtual_machine" "main" {
       publisher = var.settings.plan.publisher
     }
   }
-
-os_disk {
-  caching                   = var.settings.os_disk.caching
-  disk_size_gb              = try(var.settings.os_disk.disk_size_gb, null)
-  storage_account_type      = try(var.settings.os_disk.storage_account_type, null)
-  write_accelerator_enabled = try(var.settings.os_disk.write_accelerator_enabled, false)
-  disk_encryption_set_id    = can(var.settings.os_disk.disk_encryption_set_key) ? var.resources.disk_encryption_sets[var.settings.os_disk.disk_encryption_set_key].id : null
-  os_managed_disk_id        = try(var.settings.os_disk.create_disk, false) ? azurerm_managed_disk.creation.id : null
-}
+  os_managed_disk_id = try(var.settings.os_disk.create_disk, false) ? azurerm_managed_disk.creation.id : null
+  os_disk {
+    caching                   = var.settings.os_disk.caching
+    disk_size_gb              = try(var.settings.os_disk.disk_size_gb, null)
+    storage_account_type      = try(var.settings.os_disk.storage_account_type, null)
+    write_accelerator_enabled = try(var.settings.os_disk.write_accelerator_enabled, false)
+    disk_encryption_set_id    = can(var.settings.os_disk.disk_encryption_set_key) ? var.resources.disk_encryption_sets[var.settings.os_disk.disk_encryption_set_key].id : null
+  }
 
 
   source_image_reference {
