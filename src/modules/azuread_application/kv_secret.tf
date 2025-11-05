f8deac8bba9d28fb220e78f8875668af1becb26b
Borislav Raynov <braynov@efellows.bg>
2025-11-05 17:06:08 +0200

    Refactor secret arguments

--- src/modules/azuread_application/kv_secret.tf
+++ src/modules/azuread_application/kv_secret.tf
@@ -1,8 +1,5 @@
 resource "azurerm_key_vault_secret" "main" {
-  count = try(
-    var.settings.create_password.keyvault_ref,
-    try(var.settings.create_password.keyvault_id, null)
-  ) == null ? 0 : 1
+  count = local.key_vault_id == null ? 0 : 1
 
   name         = try(var.settings.create_password.secret_name, "${azuread_application.main.display_name}-client-secret")
   value        = azuread_application_password.main[0].value
