7101f66147c3ae8dbc162916d4ed38a8e5b8b790
Borislav Raynov <braynov@efellows.bg>
2025-11-05 17:55:43 +0200

    Change required argument reference

--- src/modules/azuread_application/kv_secret.tf
+++ src/modules/azuread_application/kv_secret.tf
@@ -4,7 +4,10 @@ resource "azurerm_key_vault_secret" "main" {
     try(var.settings.create_password.key_vault_id, null)
   ) == null ? 0 : 1
 
-  name         = try(var.settings.create_password.secret_name, "${azuread_application.main.display_name}-client-secret")
-  value        = azuread_application_password.main[0].value
-  key_vault_id = local.key_vault_id
+  name  = try(var.settings.create_password.secret_name, "${azuread_application.main.display_name}-client-secret")
+  value = azuread_application_password.main[0].value
+  key_vault_id = try(
+    module.keyvaults[var.settings.create_password.keyvault_ref].id,
+    try(var.settings.create_password.key_vault_id, null)
+  )
 }
