ace81f224f95a396ca1cc802f5b51c5887851b91
Borislav Raynov <braynov@efellows.bg>
2025-11-05 12:31:16 +0200

    Add client secret creation to AD apps

--- src/modules/azuread_application/_outputs.tf
+++ src/modules/azuread_application/_outputs.tf
@@ -1,7 +1,19 @@
+output "id" {
+  value = azuread_application.main.id
+}
+
 output "client_id" {
   value = azuread_application.main.client_id
 }
 
 output "principal_id" {
   value = azuread_application.main.object_id
+}
+
+output "pass_key_id" {
+  value = azuread_application_password.main.key_id
+}
+
+output "client_scret_value" {
+  value = azuread_application_password.main.value
 }
---
+++ src/modules/azuread_application/azuread_application_password.tf
@@ -0,0 +1,9 @@
+resource "azuread_application_password" "main" {
+  count = try(var.settings.create_password, null) == null ? 0 : 1
+
+  application_id      = azuread_application.main.id
+  display_name        = try(var.settings.create_password.display_name, null)
+  end_date            = try(var.settings.create_password.end_date, null)
+  rotate_when_changed = try(var.settings.create_password.rotate_when_changed, null)
+  start_date          = try(var.settings.create_password.start_date, null)
+}
