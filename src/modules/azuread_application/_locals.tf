0845358343e2edafea575cee05ebfd6008eeea56
Borislav Raynov <braynov@efellows.bg>
2025-11-05 17:41:46 +0200

    Change reference

--- src/modules/azuread_application/_locals.tf
+++ src/modules/azuread_application/_locals.tf
@@ -2,7 +2,7 @@ locals {
   key_vault_id = try(
     var.resources[
       try(var.settings.create_password.kv_lz_key, var.client_config.landingzone_key)
-    ].keyvaults[var.settings.create_password.keyvault_ref].id,
+    ].module.keyvaults[var.settings.create_password.keyvault_ref].id,
     try(var.settings.create_password.key_vault_id, null)
   )
 
