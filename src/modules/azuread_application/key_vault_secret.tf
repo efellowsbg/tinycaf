9f4ab87be699c704c12ef8da6bfd63403f28c265
Borislav Raynov <braynov@efellows.bg>
2025-11-05 15:15:24 +0200

    Add lz key for kv secret

--- src/modules/azuread_application/key_vault_secret.tf
+++ src/modules/azuread_application/key_vault_secret.tf
@@ -1,7 +1,7 @@
 resource "azurerm_key_vault_secret" "main" {
   count = try(
     var.settings.create_password.keyvault_ref,
-    try(var.settings.create_password.key_vault_id, null)
+    try(var.settings.create_password.keyvault_id, null)
   ) == null ? 0 : 1
 
   name         = try(var.settings.create_password.secret_name, "${azuread_application.main.display_name}-client-secret")
