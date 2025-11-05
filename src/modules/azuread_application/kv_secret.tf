14c3389dfc3200ba7a78d665c6eb8aa77ad5b9e7
Borislav Raynov <braynov@efellows.bg>
2025-11-05 17:47:56 +0200

    Change secret creation rule

--- src/modules/azuread_application/kv_secret.tf
+++ src/modules/azuread_application/kv_secret.tf
@@ -1,5 +1,8 @@
 resource "azurerm_key_vault_secret" "main" {
-  count = local.key_vault_id == null ? 0 : 1
+  count = try(
+    var.settings.create_password.keyvault_ref,
+    try(var.settings.create_password.key_vault_id, null)
+  ) == null ? 0 : 1
 
   name         = try(var.settings.create_password.secret_name, "${azuread_application.main.display_name}-client-secret")
   value        = azuread_application_password.main[0].value
