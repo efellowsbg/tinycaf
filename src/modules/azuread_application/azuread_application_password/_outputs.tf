600aeeafa65e4a1e13ede2ff53254b73f0980ea7
Borislav Raynov <braynov@efellows.bg>
2025-11-06 12:36:38 +0200

    Fix outputs

--- src/modules/azuread_application/azuread_application_password/_outputs.tf
+++ src/modules/azuread_application/azuread_application_password/_outputs.tf
@@ -1,10 +1,10 @@
 output "pass_key_id" {
-  value       = azuread_application_password.main.key_id
+  value       = azuread_application_password.main[0].key_id
   description = "Client secret key id"
 }
 
 output "client_secret_value" {
-  value       = azuread_application_password.main.value
+  value       = azuread_application_password.main[0].value
   description = "Client secret value"
   sensitive   = true
 }
--- src/modules/azuread_application/azuread_application_password/kv_secret.tf
+++ src/modules/azuread_application/azuread_application_password/kv_secret.tf
@@ -2,6 +2,6 @@ resource "azurerm_key_vault_secret" "main" {
   count = try(local.key_vault_id, null) != null ? 1 : 0
 
   name         = var.settings.create_password.secret_name
-  value        = azuread_application_password.main.client_secret_value
+  value        = azuread_application_password.main[0].value
   key_vault_id = local.key_vault_id
 }
