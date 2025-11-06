3de4de06a5f3b6dae3465e2b241f344d2ff5fd9d
Borislav Raynov <braynov@efellows.bg>
2025-11-06 12:27:13 +0200

    Fix outputs

--- src/modules/azuread_application/azuread_application_password/_outputs.tf
+++ src/modules/azuread_application/azuread_application_password/_outputs.tf
@@ -1,10 +1,10 @@
 output "pass_key_id" {
-  value       = azuread_application_password.key_id
+  value       = azuread_application_password.main.key_id
   description = "Client secret key id"
 }
 
 output "client_secret_value" {
-  value       = azuread_application_password.value
+  value       = azuread_application_password.main.value
   description = "Client secret value"
   sensitive   = true
 }
