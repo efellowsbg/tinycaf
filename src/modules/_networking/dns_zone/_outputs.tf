dba12892fdff4131d2b6663340530059e62f9609
Borislav Raynov <braynov@efellows.bg>
2025-11-07 12:56:41 +0200

    Change ouputs

--- src/modules/_networking/dns_zone/_outputs.tf
+++ src/modules/_networking/dns_zone/_outputs.tf
@@ -10,10 +10,10 @@ output "name" {
   value = azurerm_dns_zone.main.name
 }
 
-output "txt_record_id" {
-  value = azurerm_dns_txt_record.main.id
+output "txt_record_ids" {
+  value = { for k, r in azurerm_dns_txt_record.main : k => r.id }
 }
 
-output "txt_record_fqdn" {
-  value = azurerm_dns_txt_record.main.fqdn
+output "txt_record_fqdns" {
+  value = { for k, r in azurerm_dns_txt_record.main : k => r.fqdn }
 }
