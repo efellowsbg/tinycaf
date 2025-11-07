5f70bde8ca26c275fcc717b984c7c56e58ae3bf1
Borislav Raynov <braynov@efellows.bg>
2025-11-07 11:09:21 +0200

    Fix typo and kv pe dynamic block

--- src/modules/_networking/dns_zone/main.tf
+++ src/modules/_networking/dns_zone/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_dns_zone" "main" {
   name                = var.settings.name
   resource_group_name = local.resource_group_name
-  tags                = try(local.tags, null)
+  tags                = try(var.settings.azurerm_dns_zone.tags, local.tags)
 
   dynamic "soa_record" {
-    for_each = can(var.settings.identity) ? [1] : []
+    for_each = can(var.settings.soa_record) ? [1] : []
     content {
       email         = var.settings.soa_record.email
       host_name     = try(var.settings.soa_record.host_name, null)
@@ -14,7 +14,7 @@ resource "azurerm_dns_zone" "main" {
       retry_time    = try(var.settings.soa_record.retry_time, null)
       serial_number = try(var.settings.soa_record.serial_number, null)
       ttl           = try(var.settings.soa_record.ttl, null)
-      tags          = local.tags
+      tags          = try(var.settings.soa_record.tags, local.tags)
     }
   }
 }
