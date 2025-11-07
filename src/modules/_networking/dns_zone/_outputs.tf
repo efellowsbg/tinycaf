811508d841ac6ce11a5e5a1f6419312321f2985f
Borislav Raynov <braynov@efellows.bg>
2025-11-07 12:42:14 +0200

    Add dns_txt_record resource

--- src/modules/_networking/dns_zone/_outputs.tf
+++ src/modules/_networking/dns_zone/_outputs.tf
@@ -3,5 +3,17 @@ output "id" {
 }
 
 output "name_servers" {
+  value = azurerm_dns_zone.main.name_servers
+}
+
+output "name" {
   value = azurerm_dns_zone.main.name
+}
+
+output "txt_record_id" {
+  value = azurerm_dns_txt_record.main.id
+}
+
+output "txt_record_fqdn" {
+  value = azurerm_dns_txt_record.main.fqdn
 }
