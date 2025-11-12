6b375770e8ced99d92c3be33468453e1d15533fc
Borislav Raynov <braynov@efellows.bg>
2025-11-12 17:43:26 +0200

    Add references

--- src/modules/bastion_host/main.tf
+++ src/modules/bastion_host/main.tf
@@ -18,9 +18,23 @@ resource "azurerm_bastion_host" "main" {
   dynamic "ip_configuration" {
     for_each = can(var.settings.ip_configuration) ? [1] : []
     content {
-      name                 = try(var.settings.ip_configuration.name, "ip-config-${var.settings.name}")
-      subnet_id            = var.settings.ip_configuration.subnet_id
-      public_ip_address_id = var.settings.ip_configuration.public_ip_address_id
+      name = try(var.settings.ip_configuration.name, "ip-config-${var.settings.name}")
+      subnet_id = try(
+        var.resources[
+          try(var.settings.ip_configuration.sub_lz_key, var.client_config.landingzone_key)
+          ].virtual_networks[
+          split("/", var.settings.ip_configuration.subnet_ref)[0]
+          ].subnets[
+          split("/", var.settings.ip_configuration.subnet_ref)[1]
+        ].id,
+        var.settings.ip_configuration.subnet_id
+      )
+      public_ip_address_id = try(
+        var.resources[
+          try(var.settings.ip_configuration.pip_lz_key, var.client_config.landingzone_key)
+        ].public_ips[var.settings.ip_configuration.public_ip_ref].id,
+        try(var.settings.ip_configuration.public_ip_address_id, null)
+      )
     }
   }
 }
