a7f6caaabda7631e5e80021daac1987c9d3f5d4f
Borislav Raynov <braynov@efellows.bg>
2025-11-13 14:57:52 +0200

    Remove block

--- src/modules/automation_job_schedule/main.tf
+++ src/modules/automation_job_schedule/main.tf
@@ -3,16 +3,6 @@ resource "azurerm_automation_job_schedule" "main" {
   automation_account_name = try(local.automation_account_name, var.settings.automation_account_name)
   runbook_name            = try(local.runbook_name, var.settings.runbook_name)
   schedule_name           = try(local.schedule_name, var.settings.schedule_name)
+  parameters              = try(var.settings.parameters, null)
   run_on                  = try(var.settings.run_on, null)
-
-  dynamic "parameters" {
-    for_each = try(values(var.settings.parameters), {})
-    content {
-      key           = lower(parameters.value.key) # lowercase enforced
-      type          = parameters.value.type
-      mandatory     = try(parameters.value.mandatory, null)
-      position      = try(parameters.value.position, null)
-      default_value = try(parameters.value.default_value, null)
-    }
-  }
 }
