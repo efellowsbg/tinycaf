72d19881ba8a2185e26540d78b8b664199b931d3
Borislav Raynov <braynov@efellows.bg>
2025-11-13 18:04:37 +0200

    Fix tags

--- src/subscriptions.tf
+++ src/subscriptions.tf
@@ -11,9 +11,6 @@ module "subscriptions" {
   resources = merge(
     {
       (var.landingzone.key) = {
-        # resource_groups    = module.resource_groups
-        # managed_identities = module.managed_identities
-        # key_vault_keys     = module.key_vault_keys
       }
     },
     {
