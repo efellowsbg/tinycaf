ea75caaa5b2130dc297dbee708e1c4c9ec539c87
Borislav Raynov <braynov@efellows.bg>
2025-11-17 16:28:31 +0200

    Add argument

--- src/modules/azuread_administrative_unit/main.tf
+++ src/modules/azuread_administrative_unit/main.tf
@@ -1,5 +1,5 @@
 resource "azuread_administrative_unit" "main" {
-  display_name              = var.settings.description
+  display_name              = var.settings.display_name
   description               = try(var.settings.description, null)
   members                   = try(var.settings.members, null)
   hidden_membership_enabled = try(var.settings.hidden_membership_enabled, null)
