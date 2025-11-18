ba6b9be12a8ee60a3e584185fb82d084e7ee36ce
Borislav Raynov <braynov@efellows.bg>
2025-11-18 15:36:16 +0200

    Add option for primary_network_interface_id value

--- src/modules/virtual_machines/virtual_machine_default/_locals.tf
+++ src/modules/virtual_machines/virtual_machine_default/_locals.tf
@@ -2,26 +2,28 @@ locals {
   resource_group = var.resources[
     try(var.settings.lz_key, var.client_config.landingzone_key)
   ].resource_groups[var.settings.resource_group_ref]
-  resource_group_name = local.resource_group.name
-  location            = local.resource_group.location
 
+  resource_group_name   = local.resource_group.name
+  location              = local.resource_group.location
+  create_managed_disk   = try(coalesce(var.settings.storage_os_disk.create_disk, false), false)
   network_interface_ids = module.network_interface.ids
+
   key_vault_id = try(var.resources[
     try(var.settings.os_profile.keyvault_lz_key, var.client_config.landingzone_key)
     ].keyvaults[
     var.settings.os_profile.keyvault_ref
   ].id, null)
-  tags = merge(
-    var.global_settings.tags,
-    var.global_settings.inherit_resource_group_tags ? local.resource_group.tags : {},
-    try(var.settings.tags, {})
-  )
+
   identity_ids = [
     for id_ref in try(var.settings.identity.identity_ids_ref, []) :
     var.resources[
       try(var.settings.identity.managed_identity_lz_key, var.client_config.landingzone_key)
     ].managed_identities[id_ref].id
   ]
-  create_managed_disk = try(coalesce(var.settings.storage_os_disk.create_disk, false), false)
 
+  tags = merge(
+    var.global_settings.tags,
+    var.global_settings.inherit_resource_group_tags ? local.resource_group.tags : {},
+    try(var.settings.tags, {})
+  )
 }
