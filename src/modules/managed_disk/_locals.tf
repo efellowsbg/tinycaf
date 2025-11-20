27c120eec50ab10624ed7e8179af853fcdee263e
Borislav Raynov <106381177+BorislavRaynov@users.noreply.github.com>
2025-11-20 10:45:50 +0200

    [TC-85] - Update managed_disks module (#105)
    * Add module
    
    * Change reference
    
    * Remove references from locals

--- src/modules/managed_disk/_locals.tf
+++ src/modules/managed_disk/_locals.tf
@@ -5,6 +5,13 @@ locals {
   resource_group_name = local.resource_group.name
   location            = local.resource_group.location
 
+  storage_account_id = try(
+    var.resources[
+      try(var.settings.stacc_lz_key, var.client_config.landingzone_key)
+    ].storage_accounts[var.settings.storage_account_ref].id,
+    try(var.settings.storage_account_id, null)
+  )
+
   tags = merge(
     var.global_settings.tags,
     var.global_settings.inherit_resource_group_tags ? local.resource_group.tags : {},
