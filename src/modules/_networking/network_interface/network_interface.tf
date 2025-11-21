ffde175ceb9ec9c4766396bb7bc6340b4804382d
Borislav Raynov <braynov@efellows.bg>
2025-11-21 15:16:47 +0200

    Refactor module

--- src/modules/_networking/network_interface/network_interface.tf
+++ src/modules/_networking/network_interface/network_interface.tf
@@ -11,6 +11,12 @@ resource "azurerm_network_interface" "main" {
       name                          = ip_configuration.value.name
       private_ip_address_allocation = try(ip_configuration.value.private_ip_address_allocation, "Dynamic")
       private_ip_address            = try(ip_configuration.value.private_ip_address, null)
+      private_ip_address_version    = try(ip_configuration.value.private_ip_address_version, "IPv4")
+      primary                       = try(ip_configuration.value.primary, null)
+      gateway_load_balancer_frontend_ip_configuration_id = try(
+        ip_configuration.value.gateway_load_balancer_frontend_ip_configuration_id, null
+      )
+
       subnet_id = try(
         var.resources[
           try(ip_configuration.value.subnet_lz_key, var.client_config.landingzone_key)
@@ -21,17 +27,13 @@ resource "azurerm_network_interface" "main" {
         ].id,
         ip_configuration.value.subnet_id, null
       )
-      private_ip_address_version = try(ip_configuration.value.private_ip_address_version, "IPv4")
+
       public_ip_address_id = try(
         var.resources[
           try(ip_configuration.value.public_ip_lz_key, var.client_config.landingzone_key)
         ].public_ips[ip_configuration.value.public_ip_ref].id,
         ip_configuration.value.public_ip_id, null
       )
-      gateway_load_balancer_frontend_ip_configuration_id = try(
-        ip_configuration.value.gateway_load_balancer_frontend_ip_configuration_id, null
-      )
-      primary = try(ip_configuration.value.primary, null)
     }
   }
 }
