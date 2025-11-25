dde5e508e12f8c351462036650b35269ca7cdc15
Borislav Raynov <braynov@efellows.bg>
2025-11-25 07:56:38 +0200

    Remove comments

--- src/modules/role_assignments/subscription/_outputs.tf
+++ src/modules/role_assignments/subscription/_outputs.tf
@@ -1,15 +1,3 @@
-# output "subscription_assignments" {
-#   description = "Mapping of users to built-in role assignments at the subscription scope"
-#   value = {
-#     for key, assignment in azurerm_role_assignment.assignments :
-#     key => {
-#       user_principal_name = assignment.principal_id != "" ? data.azuread_user.users[local.flat_assignments[key].user].user_principal_name : null
-#       role                = local.flat_assignments[key].role
-#       role_assignment_id  = assignment.id
-#       scope               = assignment.scope
-#     }
-#   }
-# }
 output "subscription_assignments" {
   value = {
     for key, assignment in azurerm_role_assignment.assignments :
