d0e47863b046a091794e35edc18febc7219109d6
Borislav Raynov <braynov@efellows.bg>
2025-11-25 16:16:00 +0200

    Remove comments

--- src/modules/role_assignments/subscription/_outputs.tf
+++ src/modules/role_assignments/subscription/_outputs.tf
@@ -1,17 +1,3 @@
-# output "subscription_assignments" {
-#   value = {
-#     for key, assignment in azurerm_role_assignment.assignments :
-#     key => {
-#       principal_id = assignment.principal_id
-
-#       user_principal_name = (
-#         contains(keys(data.azuread_user.users), local.flat_assignments[key].user)
-#         ? data.azuread_user.users[local.flat_assignments[key].user].user_principal_name
-#         : null
-#       )
-#     }
-#   }
-# }
 output "subscription_assignments" {
   description = "Resolved subscription-level role assignments"
 
