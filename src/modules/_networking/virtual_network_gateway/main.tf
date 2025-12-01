e0013e33c6c7faa4466a26a79a55756bae4dbee1
Borislav Raynov <braynov@efellows.bg>
2025-12-01 17:07:17 +0200

    Change ip conifg argument LZ reference

--- src/modules/_networking/virtual_network_gateway/main.tf
+++ src/modules/_networking/virtual_network_gateway/main.tf
@@ -19,11 +19,11 @@ resource "azurerm_virtual_network_gateway" "main" {
       private_ip_address_allocation = try(ip_configuration.value.private_ip_address_allocation, null)
 
       public_ip_address_id = var.resources[
-        try(ip_configuration.value.lz_key, var.client_config.landingzone_key)
+        try(ip_configuration.value.lz_key, ip_configuration.value.pip_lz_key, var.client_config.landingzone_key)
       ].public_ips[ip_configuration.value.public_ip_address_ref].id
 
       subnet_id = var.resources[
-        try(ip_configuration.value.lz_key, var.client_config.landingzone_key)
+        try(ip_configuration.value.lz_key, ip_configuration.value.snet_lz_key, var.client_config.landingzone_key)
         ].virtual_networks[
         split("/", ip_configuration.value.subnet_ref)[0]
         ].subnets[
