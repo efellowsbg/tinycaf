90142be97dd5083afa8686b6fb0b2749de4375a5
Borislav Raynov <106381177+BorislavRaynov@users.noreply.github.com>
2025-12-02 09:53:02 +0200

    Update modules (#110)
    * Handle separate lz key for snet and nsg
    
    * Change ip conifg argument LZ reference
    
    * FIx fallback to expression in locals
    
    * Remove comments

--- src/modules/_networking/subnet_network_security_group_association/_locals.tf
+++ src/modules/_networking/subnet_network_security_group_association/_locals.tf
@@ -1,16 +1,28 @@
 locals {
-  subnet_id = try(
-    var.resources[
-      try(var.settings.lz_key, var.client_config.landingzone_key)
-    ].virtual_networks[split("/", var.settings.subnet_ref)[0]].subnets[split("/", var.settings.subnet_ref)[1]].id,
-    var.settings.subnet_ref
+  subnet_lz_key = (
+    try(var.settings.lz_key, null) != null ?
+    var.settings.lz_key :
+    try(var.settings.sub_lz_key, var.client_config.landingzone_key)
   )
 
+  nsg_lz_key = (
+    try(var.settings.lz_key, null) != null ?
+    var.settings.lz_key :
+    try(var.settings.nsg_lz_key, var.client_config.landingzone_key)
+  )
+
+  subnet_id = try(
+    var.resources[local.subnet_lz_key]
+    .virtual_networks[split("/", var.settings.subnet_ref)[0]]
+    .subnets[split("/", var.settings.subnet_ref)[1]]
+    .id,
+    var.settings.subnet_id
+  )
 
   network_security_group_id = try(
-    var.resources[
-      try(var.settings.lz_key, var.client_config.landingzone_key)
-    ].network_security_groups[var.settings.network_security_group_ref].id,
-    var.settings.network_security_group_ref
+    var.resources[local.nsg_lz_key]
+    .network_security_groups[var.settings.network_security_group_ref]
+    .id,
+    var.settings.network_security_group_id
   )
 }
