5164dae18e195b6e00e319bfa9eddb9e81254b3e
Lyudmil Ilchev <lilchev@efellows.bg>
2026-01-08 13:08:42 +0200

    add fix

--- src/modules/_networking/local_network_gateway/main.tf
+++ src/modules/_networking/local_network_gateway/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_local_network_gateway" "main" {
   name                = var.settings.name
   location            = local.location
   resource_group_name = local.resource_group_name
-  address_space       = var.settings.cidr
+  address_space       = try(var.settings.cidr, null)
 
   gateway_address = try(var.settings.gateway_address, null)
   gateway_fqdn    = try(var.settings.gateway_fqdn, null)
