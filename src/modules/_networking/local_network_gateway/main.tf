142ec9386fdaac600a234715e4d94fa40cdebf98
Lyudmil Ilchev <lilchev@efellows.bg>
2026-01-08 12:19:26 +0200

    Add bgp settings for local network gaterway

--- src/modules/_networking/local_network_gateway/main.tf
+++ src/modules/_networking/local_network_gateway/main.tf
@@ -6,4 +6,14 @@ resource "azurerm_local_network_gateway" "main" {
 
   gateway_address = try(var.settings.gateway_address, null)
   gateway_fqdn    = try(var.settings.gateway_fqdn, null)
+
+  dynamic "bgp_settings" {
+    for_each = can(var.settings.bgp_settings) && var.settings.bgp_settings != null ? [1] : []
+
+    content {
+      asn                 = var.settings.bgp_settings.asn
+      bgp_peering_address = var.settings.bgp_settings.bgp_peering_address
+      peer_weight = try(var.settings.bgp_settings.peer_weight, null)
+    }
+  }
 }
