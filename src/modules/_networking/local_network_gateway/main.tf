97e8835ccb543e8a2df69dd166be9b818c410da5
Lyudmil Ilchev <124342933+lyudmililchev92@users.noreply.github.com>
2026-01-08 13:24:16 +0200

    Add bgp settings in local network gateway (#117)
    * Add bgp settings for local network gaterway
    
    * add fix
    
    * fix bgp settings

--- src/modules/_networking/local_network_gateway/main.tf
+++ src/modules/_networking/local_network_gateway/main.tf
@@ -2,8 +2,18 @@ resource "azurerm_local_network_gateway" "main" {
   name                = var.settings.name
   location            = local.location
   resource_group_name = local.resource_group_name
-  address_space       = var.settings.cidr
+  address_space       = try(var.settings.cidr, null)
 
   gateway_address = try(var.settings.gateway_address, null)
   gateway_fqdn    = try(var.settings.gateway_fqdn, null)
+
+  dynamic "bgp_settings" {
+    for_each = can(var.settings.bgp_settings) ? [1] : []
+
+    content {
+      asn                 = var.settings.bgp_settings.asn
+      bgp_peering_address = var.settings.bgp_settings.bgp_peering_address
+      peer_weight = try(var.settings.bgp_settings.peer_weight, null)
+    }
+  }
 }
