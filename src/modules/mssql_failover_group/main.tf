e0021d3d6404a5524f48a797d1c2eaff476f4444
Lyudmil Ilchev <lilchev@efellows.bg>
2026-01-20 16:20:07 +0200

    fix automatic and manual blocks

--- src/modules/mssql_failover_group/main.tf
+++ src/modules/mssql_failover_group/main.tf
@@ -8,9 +8,29 @@ resource "azurerm_mssql_failover_group" "main" {
 
   databases = local.database_ids
 
-  read_write_endpoint_failover_policy {
-    mode          = try(var.settings.read_write_endpoint_failover_policy.mode, "Automatic")
-    grace_minutes = try(var.settings.read_write_endpoint_failover_policy.grace_minutes, 60)
+  dynamic "read_write_endpoint_failover_policy" {
+    for_each = (
+      try(var.settings.read_write_endpoint_failover_policy.mode, "Automatic") == "Manual"
+      ? [1]
+      : []
+    )
+
+    content {
+      mode = "Manual"
+    }
+  }
+
+  dynamic "read_write_endpoint_failover_policy" {
+    for_each = (
+      try(var.settings.read_write_endpoint_failover_policy.mode, "Automatic") != "Manual"
+      ? [1]
+      : []
+    )
+
+    content {
+      mode          = try(var.settings.read_write_endpoint_failover_policy.mode, "Automatic")
+      grace_minutes = try(var.settings.read_write_endpoint_failover_policy.grace_minutes, 60)
+    }
   }
   readonly_endpoint_failover_policy_enabled = try(
     var.settings.readonly_endpoint_failover_policy.enabled,
