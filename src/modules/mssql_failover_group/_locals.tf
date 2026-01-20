750533f4928fdb56013989cecdf7593e529a3e46
Lyudmil Ilchev <lilchev@efellows.bg>
2026-01-20 15:49:28 +0200

    add failover group module

--- src/modules/mssql_failover_group/_locals.tf
+++ src/modules/mssql_failover_group/_locals.tf
@@ -3,12 +3,18 @@ locals {
     try(var.settings.primary.lz_key, var.client_config.landingzone_key)
   ].mssql_servers[var.settings.primary.server_ref]
 
-  secondary_server = var.resources[
-    try(var.settings.secondary.lz_key, var.client_config.landingzone_key)
-  ].mssql_servers[var.settings.secondary.server_ref]
+  secondary_server = try(
+    var.resources[
+      try(var.settings.secondary.lz_key, var.client_config.landingzone_key)
+    ].mssql_servers[var.settings.secondary.server_ref],
+    null
+  )
 
-  primary_server_id   = local.primary_server.id
-  secondary_server_id = local.secondary_server.id
+  primary_server_id = local.primary_server.id
+  secondary_server_id = try(
+    local.secondary_server.id,
+    var.settings.secondary.server_id
+  )
 
   database_ids = [
     for db_ref in sort(try(var.settings.database_refs, [])) :
