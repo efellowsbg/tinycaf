4cb1b21c58020dfa1dfa755d5a23e823b4a13646
Borislav Raynov <braynov@efellows.bg>
2026-03-27 17:49:21 +0200

    Add reference

--- src/modules/automation_job_schedule/_outputs.tf
+++ src/modules/automation_job_schedule/_outputs.tf
@@ -2,6 +2,10 @@ output "id" {
   value = azurerm_automation_job_schedule.main.id
 }
 
+output "name" {
+  value = azurerm_automation_job_schedule.main.name
+}
+
 output "job_schedule_id" {
   value = azurerm_automation_job_schedule.main.job_schedule_id
 }
