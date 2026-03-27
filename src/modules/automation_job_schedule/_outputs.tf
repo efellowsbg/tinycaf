5e5cd62f81e47e05347950e4a3caf43a1b9b17bd
Borislav Raynov <braynov@efellows.bg>
2026-03-27 18:01:11 +0200

    Remove output argument

--- src/modules/automation_job_schedule/_outputs.tf
+++ src/modules/automation_job_schedule/_outputs.tf
@@ -2,10 +2,6 @@ output "id" {
   value = azurerm_automation_job_schedule.main.id
 }
 
-output "name" {
-  value = azurerm_automation_job_schedule.main.name
-}
-
 output "job_schedule_id" {
   value = azurerm_automation_job_schedule.main.job_schedule_id
 }
