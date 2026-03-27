1923f3967d31945149d99de7ab6dc3fb260ad13d
Borislav Raynov <braynov@efellows.bg>
2026-03-27 09:31:02 +0200

    Change content_uri to optional

--- src/modules/automation_account/python_packages.tf
+++ src/modules/automation_account/python_packages.tf
@@ -4,7 +4,7 @@ resource "azurerm_automation_python3_package" "main" {
   name                    = try(each.value.name, each.key)
   resource_group_name     = try(local.resource_group_name, var.settings.resource_group_name)
   automation_account_name = azurerm_automation_account.main.name
-  content_uri             = each.value.content_uri
+  content_uri             = try(each.value.content_uri, null)
   content_version         = try(each.value.content_version, null)
   hash_algorithm          = try(each.value.hash_algorithm, null)
   hash_value              = try(each.value.hash_value, null)
