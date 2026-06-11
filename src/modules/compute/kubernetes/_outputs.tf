d73ebf3608a1eca682d29a50a628bfe08561a95c
Nikolay Dosev <ndosev@efellows.bg>
2026-06-11 16:02:45 +0300

    Expose AKS identities and support them as role-assignment principals

--- src/modules/compute/kubernetes/_outputs.tf
+++ src/modules/compute/kubernetes/_outputs.tf
@@ -1,3 +1,11 @@
 output "id" {
   value = module.kubernetes_cluster.id
 }
+
+output "identity" {
+  value = module.kubernetes_cluster.identity
+}
+
+output "kubelet_identity" {
+  value = module.kubernetes_cluster.kubelet_identity
+}
